f4c0fd08441f98a60ed739749f09ff4171167780
lesa3111 <amael.le.squin@usherbrooke.ca>
2021-07-16 16:34:36 +0200

    Added graphics and shiny app

--- toyExample/dummyData_toy.R
+++ toyExample/dummyData_toy.R
@@ -9,6 +9,7 @@ graphics.off()
 library(data.table)
 library(bayesplot)
 library(cmdstanr)
+library(shiny)
 
 options(max.print = 500)
 ###########################################################?
@@ -24,10 +25,14 @@ set.seed(1969-08-18) # Woodstock seed
 n_plots = 4
 
 # Number of census per plot
-record_min = 4
-record_max = 8
+record_min = 3
+record_max = 6
 
 n_census_per_plot = sample(x = record_min:record_max, size = n_plots, replace = TRUE)
+# n_census_per_plot = rep(3, n_plots)
+
+if (any(n_census_per_plot < 2))
+	stop("There should be at least 2 census per plot")
 
 # Number of individuals per plot
 indiv_min = 10
@@ -49,8 +54,8 @@ quad_slope_precip = -0.00001
 
 slope_dbh = 1.1
 
-sigma_process = 4
-sigma_measure = 1
+sigma_process = 10
+sigma_measure = 4
 
 #### Generate complete data
 init_year_plot = sample(1990:2005, size = n_plots, replace = TRUE)
@@ -71,6 +76,7 @@ for (xy in 1:n_plots)
 	initial_dbh = rgamma(n_indiv_per_plot[xy], shape = 150^2/300, rate = 150/300)
 	index = seq(from = 1, to = nb_measures_plot, by = n_census_per_plot[xy]) + count
 	treeStates_dt[index, true_dbh := initial_dbh]
+	treeStates_dt[index, true_dbh_noError := initial_dbh]
 
 	for (j in 1:(n_census_per_plot[xy] - 1))
 	{
@@ -78,6 +84,7 @@ for (xy in 1:n_plots)
 			slope_precip*treeStates_dt[index + j - 1, precipitations] +
 			quad_slope_precip*(treeStates_dt[index + j - 1, precipitations])^2
 		treeStates_dt[index + j, true_dbh := rnorm(n = n_indiv_per_plot[xy], mean = next_dbh, sd = sigma_process)]
+		treeStates_dt[index + j, true_dbh_noError := next_dbh]
 	}
 	
 	count = count + nb_measures_plot
@@ -86,13 +93,26 @@ for (xy in 1:n_plots)
 treeStates_dt[, range(true_dbh)]
 
 treeStates_dt[, dbh := rnorm(n = .N, mean = true_dbh, sd = sigma_measure)]
+setorder(treeStates_dt, plot_id, tree_id, year)
+treeStates_dt[, unique_id := 1:.N]
 
 #### Generate partial data set (keep only first and last measurements)
 kept_rows = sort(c(treeStates_dt[, .I[which.min(year)], by = .(plot_id, tree_id)][, V1],
 	treeStates_dt[, .I[which.max(year)], by = .(plot_id, tree_id)][, V1]))
+
 treeData = treeStates_dt[kept_rows]
+print(paste0(round(treeData[, .N]*100/treeStates_dt[, .N], 2), " % of the data kept"))
+
 setorder(treeData, plot_id, tree_id, year)
 
+#### plots
+plot(treeStates_dt[, unique_id], treeStates_dt[, true_dbh], pch = 19, cex = 0.7, col = "red", ty = "o", xlab = "unique id", ylab = expression(dbh[i](t)),
+	ylim = c(min(treeStates_dt[, .(true_dbh, dbh)]) - abs(min(treeStates_dt[, .(true_dbh, dbh)]))/10,
+		max(treeStates_dt[, .(true_dbh, dbh)] + max(treeStates_dt[, .(true_dbh, dbh)]/10))), las = 1)
+points(treeData[, unique_id], treeData[, dbh], pch = 3, cex = 0.8, col = "blue", ty = "o", lty = 3)
+
+legend("top", legend = c("Obs. (with missing data)", "True states"), pch = c(3, 19),
+	col = c("blue", "red"), lty = c(3, 1), horiz=TRUE, bty="n", cex=0.9)
 
 
 ###########################################################?
@@ -223,6 +243,115 @@ init_fun = function(...)
 	return(list(Y_generated = Y_gen))
 }
 
+## A hint on how the priors look like
+# Define UI for sliders
+library(shiny)
+ui = fluidPage(
+	# ---- App title
+	titlePanel("Priors preview"),
+
+	# ---- Sidebar layout with input and output definitions
+	sidebarLayout(
+		# ---- Sidebar to demonstrate various slider options
+		sidebarPanel(
+			# ---- Input: Mean associated to normal distribution plot
+			sliderInput("mean_dnorm", "mean (normal distrib):",
+				min = -50, max = 50,
+				value = 0, step = 0.1),
+
+			# ---- Input: Std deviation associated to normal distribution plot
+			sliderInput("sd_dnorm", "standard deviation (normal distrib):",
+				min = 0, max = 20,
+				value = 10, step = 0.01),
+
+			# ---- Input: Range to plot normal distribution
+			sliderInput("range_dnorm", "Range normal:",
+				min = -80, max = 80,
+				value = c(-40, 40)),
+			
+			# ---- Input: Mean associated to normal distribution plot
+			sliderInput("mean_dgamma", "mean (gamma distrib):",
+				min = 0, max = 200,
+				value = 5, step = 0.1),
+
+			# ---- Input: Std deviation associated to normal distribution plot
+			sliderInput("var_dgamma", "variance (gamma distrib):",
+				min = 0, max = 200,
+				value = 5, step = 0.1),
+
+			# ---- Input: Range to plot gamma distribution
+			sliderInput("range_dgamma", "Range gamma:",
+				min = 0, max = 200,
+				value = c(0, 20)),
+
+			# ---- Input: Integration range
+			sliderInput("integration_range", "a: P(X < a):",
+				min = 0, max = 200,
+				value = 1, step = 0.1)
+		),
+
+		# ---- Main panel for displaying outputs
+		mainPanel(
+			# ---- Output: Plot of the distributions and display shape/scale for gamma distrib
+			tableOutput("values"),
+			plotOutput(outputId = "normal_pdf"),
+			plotOutput(outputId = "gamma_pdf")
+		)
+	)
+)
+
+## Define server logic for slider examples
+server = function(input, output) {
+	# --- Reactive expression to display the value of shape and rate for gamma distrib
+	sliderValues = reactive({
+	data.frame(
+	Name = c("Gamma distribution, shape:", "Gamma distribution, rate:", "P(x < a), gamma distrib:"),
+	Value = as.character(c(
+		paste(input$mean_dgamma, "^2/", input$var_dgamma, sep = ""),
+		paste(input$mean_dgamma, "/", input$var_dgamma, sep = ""),
+		integrate(
+			function(x){return (dgamma(x, shape = input$mean_dgamma^2/input$var_dgamma, rate = input$mean_dgamma/input$var_dgamma))},
+			lower = 0, upper = input$integration_range)$value)),
+	stringsAsFactors = FALSE)
+	})
+
+	# ---- Show the values in an HTML table
+	output$values <- renderTable({sliderValues()})
+
+	output$normal_pdf = renderPlot({curve(dnorm(x, input$mean_dnorm, input$sd_dnorm),
+		input$range_dnorm[1], input$range_dnorm[2], lwd = 5, col = "#0072B5",
+		ylab = "Normal pdf")})
+
+	output$gamma_pdf = renderPlot({
+		curve(dgamma(x,
+			shape = input$mean_dgamma^2/input$var_dgamma, rate = input$mean_dgamma/input$var_dgamma),
+			input$range_dgamma[1], input$range_dgamma[2], lwd = 5, col = "#363945",
+			ylab = "Gamma pdf")
+
+		sh <<- input$mean_dgamma^2/input$var_dgamma # https://stackoverflow.com/questions/2628621/how-do-you-use-scoping-assignment-in-r
+		ra <<- input$mean_dgamma/input$var_dgamma
+
+		DescTools::Shade(
+			dgamma(x, shape = sh, rate = ra),
+			breaks = c(0, input$integration_range), col = "#A1A1A166", density = NA)
+		})
+}
+
+shinyApp(ui, server)
+
+
+inp = list(range_dgamma = c(5, 10))
+lala = function(x, inp) {return (dgamma(x, shape = inp$range_dgamma[1], rate = 1))}
+
+curve(dgamma(x, shape = 5^2/5, rate = 5/5),
+			0, 5, lwd = 5, col = "#363945",
+			ylab = "Gamma pdf")
+
+		DescTools::Shade(
+			lala(x, inp),
+			breaks = c(1, 2), col = "#A1A1A166", density = NA)
+
+
 #### Load data
 ## Data are already in memory
 precip = precipitations_yearly
@@ -251,7 +380,7 @@ n_obs = treeData[, .N]
 print(paste("Number of data:", n_obs))
 
 n_indiv = unique(treeData[, .(tree_id, plot_id)])[, .N]
-print(paste("Number of individuals:", treeData[, .N]))
+print(paste("Number of individuals:", n_indiv))
 
 nbYearsPerIndiv = unique(indices[, .(tree_id, plot_id, nbYearsPerIndiv)])[, nbYearsPerIndiv]
 if (length(nbYearsPerIndiv) != n_indiv)
@@ -272,10 +401,12 @@ if (length(children_index) != n_obs - n_indiv)
 if (length(not_parent_index) != indices[.N, index_gen] - n_indiv)
 	stop("Dimension mismatch between not_parent_index, n_hiddenState, and n_indiv")
 
+Y_generated_0 = rnorm(n_indiv, treeData[parents_index, dbh], 5)
+
 #### Stan model
 ## Define stan variables
 # Common variables
-maxIter = 1e4
+maxIter = 4e3
 n_chains = 4
 
 # Data to provide
@@ -295,6 +426,9 @@ stanData = list(
 	childrenObs_index = indices[type == "child", index_gen], # Corresponding index of observed children in Y_generated
 	climate_index = indices[type == "parent", index_precip_start], # Index of the climate associated to each parent
 	not_parent_index = not_parent_index, # Index in Y_generated of states that cannot be compared to data
+
+	# Initial state, used to start the states and compare with Y_generated[parentsObs_index] (i.e., a prior parameter)
+	Y_generated_0 = Y_generated_0,
 
 	# Observations
 	Yobs = treeData[, dbh],
@@ -325,9 +459,9 @@ for (i in 1:n_chains)
 ## Compile stan model
 # model = stan_model(file = "./toy.stan")
 # model = cmdstan_model("toyPara_reduce_sum.stan", cpp_options = list(stan_threads = TRUE)) # list(stan_threads = TRUE, stan_opencl = TRUE)
-# model = cmdstan_model("toyPara_GPUs.stan") #, cpp_options = list(stan_opencl = TRUE))
+# model = cmdstan_model("toyPara_GPUs.stan", cpp_options = list(stan_opencl = TRUE))
 
-model = cmdstan_model("toyPara_GPUs.stan", cpp_options = list(stan_threads = TRUE))
+model = cmdstan_model("toyPara_GPUs.stan")
 
 ## Run model
 start = proc.time()
@@ -338,8 +472,9 @@ start = proc.time()
 # results = model$sample(data = stanData, parallel_chains = n_chains, threads_per_chain = 8, refresh = 2,
 # 	iter_warmup = maxIter/2, iter_sampling = maxIter/2, chains = n_chains, init = initVal_Y_gen)
 
-results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 1000, chains = n_chains,
-	threads_per_chain = 2, iter_warmup = maxIter/2, iter_sampling = maxIter/2, init = initVal_Y_gen)
+# maxIter = 2
+results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 400, chains = n_chains,
+	iter_warmup = maxIter/2, iter_sampling = maxIter/2, init = initVal_Y_gen, max_treedepth = 14, adapt_delta = 0.95)
 
 # results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 2, chains = n_chains,
 # 	opencl_ids = c(0, 0), iter_warmup = maxIter/2, iter_sampling = maxIter/2, init = initVal_Y_gen)
@@ -347,11 +482,15 @@ results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 10
 proc.time() - start
 
 results
+results$cmdstan_diagnose()
 
 plot_title = ggplot2::ggtitle("Posterior distributions", "with medians and 80% intervals")
+mcmc_areas(results$draws("quad_slopes_precip"), prob = 0.8) + plot_title + ggplot2::geom_vline(quad_slope_precip)
+
+plot_title = ggplot2::ggtitle("Traces for intercept")
+mcmc_trace(results$draws("intercepts")) + plot_title
 
 # "slopes_dbh", "slopes_precip", "quad_slopes_precip", "processError", "measureError"
-mcmc_areas(results$draws("intercepts")) + plot_title
 
 # intercept = 2.4
 
@@ -362,3 +501,47 @@ mcmc_areas(results$draws("intercepts")) + plot_title
 
 # sigma_process = 4
 # sigma_measure = 1
+
+# Colours from https://www.w3schools.com/colors/colors_2021.asp
+
+# classic_blue = "#34568B"
+# flame_scarlet = "#CD212A"
+# saffron = "#FFA500"
+# biscay_green = "#56C6A9"
+# chive = "#4B5335"
+# faded_denim = "#798EA4"
+# orange_peel = "#FA7A35"
+# mosaic_blue = "#00758F"
+# sunlight = "#EDD59E"
+# coral_pink = "#E8A798"
+# cinnamon_stick = "#9C4722"
+# grape_compote = "#6B5876"
+# lark = "#B89B72"
+# navy_blazer = "#282D3C"
+# brilliant_white = "#EDF1FF"
+# ash = "#A09998"
+# amberglow = "#DC793E"
+# samba = "#A2242F"
+# sandstone = "#C48A69"
+# green_sheen = "#D9CE52"
+# rose_tan = "#D19C97"
+# ultramarine_green = "#006B54"
+# fired_brick = "#6A2E2A"
+# peach_nougat = "#E6AF91"
+# magenta_purple = "#6C244C"
+
+# marigold = "#FDAC53"
+# cerulean = "#9BB7D4"
+# rust = "#B55A30"
+# illuminating = "#F5DF4D"
+# french_blue = "#0072B5"
+# green_ash = "#A0DAA9"
+# burnt_coral = "#E9897E"
+# mint = "#00A170"
+# amethyst_orchid = "#926AA6"
+# raspberry_sorbet = "#D2386C"
+# inkwell = "#363945"
+# ultimate_gray = "#939597"
+# buttercream = "#EFE1CE"
+# desert_mist = "#E0B589"
+# willow = "#9A8B4F"
