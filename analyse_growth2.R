49db9ab68d4570fef70206a6e60f0db3a0c89914
lesa3111 <amael.le.squin@usherbrooke.ca>
2021-12-06 18:41:18 +0100

    Added graphs for posterior distributions + traces; vectorised the generation data to compute residuals

--- analyse_growth2.R
+++ analyse_growth2.R
@@ -8,6 +8,7 @@ graphics.off()
 options(max.print = 500)
 
 library(data.table)
+library(bayesplot)
 library(cmdstanr)
 library(stringi)
 library(DHARMa)
@@ -85,14 +86,16 @@ getLastRun = function(path, begin = "growth-", extension = ".rds", format = "ymd
 
 	dt[stri_detect(str = day, regex = extension), day := stri_sub(str = day, to = stri_locate_first(str = day, regex = "_")[,"start"] - 1)]
 	setorder(dt, year, month, day)
-	return (dt[.N, file])
+	return (list(file = dt[.N, file], time_ended = paste(dt[.N, year], dt[.N, month], dt[.N, day], sep = "-")))
 }
 
 #### Read data
 ## Common variables
 species = "Tilia_platyphyllos"
 path = paste0("./", species, "/")
-lastRun = getLastRun(path)
+info_lastRun = getLastRun(path)
+lastRun = info_lastRun[["file"]]
+time_ended = info_lastRun[["time_ended"]]
 
 isPrecip_normalised = TRUE
 isDBH_normalised = TRUE
@@ -103,13 +106,13 @@ results = readRDS(paste0(path, lastRun))
 if (isPrecip_normalised)
 {
 	print("Precip parameter must be transformed when working on the real precip scale")
-	norm_clim_dt = readRDS("Tilia_platyphyllos/climate_normalisation.rds")
+	norm_clim_dt = readRDS(paste0(path, "climate_normalisation.rds"))
 }
 
 if (isDBH_normalised)
 {
 	print("DBH must be transformed when working on the real DBH scale")
-	norm_dbh_dt = readRDS("Tilia_platyphyllos/dbh_normalisation.rds")
+	norm_dbh_dt = readRDS(paste0(path, "dbh_normalisation.rds"))
 }
 
 #### Get parameters
@@ -140,31 +143,27 @@ indices = readRDS(paste0(treeFolder, species, "_indices.rds"))
 indices = unique(indices[, .(tree_id, pointInventory_id, index_clim_start, index_clim_end)])
 
 ## Create simulations
-n_rep = 25
+n_rep = 500
 dt = data.table(rep_dbh_end = rep(dbh_end, each = n_rep), sampled = numeric(n_rep * length(dbh_end)))
 if (isDBH_normalised)
 	dt[, rep_dbh_end := rep_dbh_end/norm_dbh_dt[, sd]]
 
-count = 0
-
 for (i in 1:length(dbh_end))
 {
-	for (iter in 1:n_rep)
+	currentDbh = dbh_start[i]/ifelse(isDBH_normalised, norm_dbh_dt[, sd], 1)
+	currentClim_index = indices[i, index_clim_start]
+	for (yr in 1:delta_t[i])
 	{
-		currentDbh = dbh_start[i]/ifelse(isDBH_normalised, norm_dbh_dt[, sd], 1)
-		currentClim_index = indices[i, index_clim_start]
-		for (yr in 1:delta_t[i])
-		{
-			precip = climate[currentClim_index, pr]
-			currentDbh = rnorm(1, rnorm(1, nextDBH(currentDbh, precip, meanParams, norm_clim_dt), processError), 0.05)
-			currentClim_index = currentClim_index + 1
-		}
-		count = count + 1
-		dt[count, sampled := currentDbh]
+		precip = climate[currentClim_index, pr]
+		currentDbh = rnorm(n_rep, rnorm(n_rep, nextDBH(currentDbh, precip, meanParams, norm_clim_dt), processError), 0.006)
+		currentClim_index = currentClim_index + 1
 	}
-	if (i %% 20 == 0)
+	dt[((i - 1)*n_rep + 1):(i*n_rep), sampled := currentDbh]
+	if (i %% 100 == 0)
 		print(paste0(round(i*100/length(dbh_end), 2), "% done"))
 }
+
+saveRDS(dt, "testForResiduals.rds")
 
 ## Reshape simulations into a matrix length(dbh) x n_rep
 sims = matrix(data = dt[, sampled], nrow = n_rep, ncol = length(dbh_end)) # each column is for one data point (the way I organised the dt)
@@ -177,22 +176,88 @@ forDharma = createDHARMa(simulatedResponse = sims,
 plot(forDharma)
 dev.off()
 
-####! CRASH TEST ZONE
-treeFolder = "~/projects/def-dgravel/amael/postdoc/bayForDemo/BayForDemo Inventories/FR IFN/processed data/"
-treeData = readRDS(paste0(treeFolder, "trees_forest_reshaped.rds"))
-treeData = treeData[speciesName_sci == "Tilia_platyphyllos"]
+#### Plot posterior distributions and traces of parameters
+# Folder to save plots
+figurePath = paste0(path, time_ended, "/")
+if (!dir.exists(figurePath))
+	dir.create(figurePath)
 
+# Intercept
+plot_title = ggplot2::ggtitle("Posterior distribution potentialMaxGrowth", "with medians and 80% intervals")
+temp_plot = mcmc_areas(results$draws("potentialMaxGrowth"), prob = 0.8) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "pmg-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+plot_title = ggplot2::ggtitle("Traces for potentialMaxGrowth")
+temp_plot = mcmc_trace(results$draws("potentialMaxGrowth")) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "pmg-traces.pdf"), plot = temp_plot, device = "pdf")
+
+# Slopes_dbh
+plot_title = ggplot2::ggtitle("Posterior distribution power_dbh", "with medians and 80% intervals")
+temp_plot = mcmc_areas(results$draws("power_dbh"), prob = 0.8) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "power_dbh-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+plot_title = ggplot2::ggtitle("Traces for power_dbh")
+temp_plot = mcmc_trace(results$draws("power_dbh")) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "power_dbh-traces.pdf"), plot = temp_plot, device = "pdf")
+
+# Slopes_precip
+plot_title = ggplot2::ggtitle("Posterior distribution optimal_precip", "with medians and 80% intervals")
+temp_plot = mcmc_areas(results$draws("optimal_precip"), prob = 0.8) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "optimal_precip-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+plot_title = ggplot2::ggtitle("Traces for optimal_precip")
+temp_plot = mcmc_trace(results$draws("optimal_precip")) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "optimal_precip-traces.pdf"), plot = temp_plot, device = "pdf")
+
+# Quad_slopes_precip
+plot_title = ggplot2::ggtitle("Posterior distribution width_precip_niche", "with medians and 80% intervals")
+temp_plot = mcmc_areas(results$draws("width_precip_niche"), prob = 0.8) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "width_precip_niche-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+plot_title = ggplot2::ggtitle("Traces for width_precip_niche")
+temp_plot = mcmc_trace(results$draws("width_precip_niche")) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "width_precip_niche-traces.pdf"), plot = temp_plot, device = "pdf")
+
+# processError
+plot_title = ggplot2::ggtitle("Posterior distribution processError", "with medians and 80% intervals")
+temp_plot = mcmc_areas(results$draws("processError"), prob = 0.8) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "processError-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+plot_title = ggplot2::ggtitle("Traces for processError")
+temp_plot = mcmc_trace(results$draws("processError")) + plot_title
+ggplot2::ggsave(filename = paste0(figurePath, "processError-traces.pdf"), plot = temp_plot, device = "pdf")
+
+# # measureError
+# plot_title = ggplot2::ggtitle("Posterior distribution measureError", "with medians and 80% intervals")
+# temp_plot = mcmc_areas(results$draws("measureError"), prob = 0.8) + plot_title
+# ggplot2::ggsave(filename = paste0(figurePath, "measureError-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+# plot_title = ggplot2::ggtitle("Traces for measureError")
+# temp_plot = mcmc_trace(results$draws("measureError")) + plot_title
+# ggplot2::ggsave(filename = paste0(figurePath, "measureError-traces.pdf"), plot = temp_plot, device = "pdf")
+
+for (i in c(1:6, 12:14))
+{
+	plot_title = ggplot2::ggtitle(paste0("Posterior distribution latent_dbh[", i, "]"),
+		"with medians and 80% intervals")
+	temp_plot = mcmc_areas(results$draws(paste0("latent_dbh[", i, "]")), prob = 0.8) + plot_title
+	ggplot2::ggsave(filename = paste0(figurePath, "latent_dbh_", i, "-distrib.pdf"), plot = temp_plot, device = "pdf")
+
+	plot_title = ggplot2::ggtitle(paste0("Traces for latent_dbh[", i, "]"))
+	temp_plot = mcmc_trace(results$draws(paste0("latent_dbh[", i, "]"), inc_warmup = TRUE)) + plot_title
+	ggplot2::ggsave(filename = paste0(figurePath, "latent_dbh_", i, "-traces.pdf"), plot = temp_plot, device = "pdf")
+}
+
+####! CRASH TEST ZONE
 latent_1_6 = getParams(results, paste0("latent_dbh[", 1:6, "]"))
 x = 2000:2005
-test = lm(latent_1_6 ~ x)
-summary(test)
 
-jpeg("./latent_real_2ndOption.jpg", height = 1080, width = 1080, quality = 100)
+pdf("./latent_real_2ndOption.pdf", height = 7, width = 7)
+op <- par(mar = c(2.5, 2.5, 0.8, 0.8), mgp = c(1.5, 0.5, 0), tck = -0.015)
 plot(2000:2005, norm_dbh_dt[, sd]*latent_1_6, pch = 19, col = "#34568B", cex = 4,
 	xlab = "Year", ylab = "Diameter at breast height (scaled)")
 points(x = 2000, y = treeData[1, dbh], pch = 19, col = "#FA7A35", cex = 2)
 points(x = 2005, y = treeData[2, dbh], pch = 19, col = "#CD212A", cex = 2)
-abline(test)
 dev.off()
 
 pdf("./growth_dbh.pdf", height = 7, width = 7)
