e9e8bbedfd8b34fdb5856900beeb8ee7068ba7e0
lesa3111 <amael.le.squin@usherbrooke.ca>
2021-12-09 12:19:35 +0100

    Estimate the parameters

--- dharma_test/fitData.R
+++ dharma_test/fitData.R
@@ -15,9 +15,17 @@ library(DHARMa)
 dt = readRDS("./data.rds")
 
 ## Create stan data
+n_data = dt[, .N]
 n_group = length(unique(dt[, group]))
+data_per_group = dt[, .N, by = group][, N]
 
-stanData = list()
+stanData = list(
+	n_data = n_data,
+	n_group = n_group,
+	data_per_group = data_per_group,
+	x = dt[, x],
+	y = dt[, y]
+)
 
 #### Run
 ## Parameters
@@ -29,14 +37,25 @@ model = cmdstan_model("./model.stan")
 
 ## Run model
 results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 200, chains = n_chains,
-	iter_warmup = maxIter/2, iter_sampling = maxIter/2, save_warmup = TRUE, init = initVal_Y_gen,
+	iter_warmup = maxIter/2, iter_sampling = maxIter/2, save_warmup = TRUE,
 	max_treedepth = 10, adapt_delta = 0.8)
 
-## Saving
-time_ended = format(Sys.time(), "%Y-%m-%d_%Hh%M")
-results$save_output_files(dir = savingPath, basename = paste0("growth-", time_ended), timestamp = FALSE, random = TRUE)
-results$save_object(file = paste0(savingPath, "growth-", time_ended, ".rds"))
-
 diagnose = results$cmdstan_diagnose()
+
+#### Check the fit
+estimatedParameters = c(beta0 = mean(results$draws("beta0")),
+	sigma_beta = mean(results$draws("sigma_beta")),
+	beta0_1 = 0,
+	beta0_2 = 0,
+	beta0_3 = 0,
+	beta0_4 = 0,
+	beta1 = mean(results$draws("beta1")),
+	sigma_res = mean(results$draws("sigma_res")))
+for (i in 1:n_group)
+	estimatedParameters[paste0("beta0_", i)] = mean(results$draws(paste0("beta0_group[", i, "]")))
+
+realParameters = readRDS("./realParameters.rds")
+print(realParameters)
+print(estimatedParameters)
 
 #### Analysing residuals
