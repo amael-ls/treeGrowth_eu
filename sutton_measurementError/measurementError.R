cf528d20a3c080ca66c82f6fe0e290175ddd821c
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-01-26 16:21:14 +0100

    Tracking script to estimate measurement errors from Sutton, Canada

---
+++ sutton_measurementError/measurementError.R
@@ -0,0 +1,9 @@
+
+#### Aim of prog: Investigate on the measurement error
+## Explanations
+# Around 950 trees have been measured by two people in Sutton Canada. It was done this way:
+#	1. The first person measure many trees (more than a hundred in a row) while the second person takes notes
+#	2. Then, the roles are reversed: person 2 measures and person 1 takes notes.
+# There should have enough measurements in a row to make sure that the second person is not influenced by the first
+#
+
