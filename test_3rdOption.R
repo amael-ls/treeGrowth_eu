1dcba687e7c02c30dd4c1eb442b8882046e33f46
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-03-08 16:01:58 +0100

    Improved function lazyTrace: labels can be added to values plotted

--- test_3rdOption.R
+++ test_3rdOption.R
@@ -101,8 +101,8 @@ growth_fct = function(dbh0, precip, params)
 ## Bayesplot is having troubles on my mac (Arial font not always found), so I create my own traces plot
 lazyTrace = function(draws, ...)
 {
-	if (!all(class(draws) %in% c("draws_array", "draws", "array")))
-		stop("The class of draws should be compatible with stan (draws_array, draws, array)")
+	if (!is.array(draws) & !all(class(draws) %in% c("draws_array", "draws", "array")))
+		stop("The class of draws should be either array, or compatible with cmdstanr (draws_array, draws, array)")
 	
 	n_chains = dim(draws)[2]
 	n_iter = dim(draws)[1]
@@ -120,23 +120,43 @@ lazyTrace = function(draws, ...)
 	xlab_ind = (ls_names == "xlab")
 	ylab_ind = (ls_names == "ylab")
 	main_ind = (ls_names == "main")
+	label_ind = stri_detect(str = ls_names, regex = "label")
 
 	scaling_ind = (ls_names == "scaling")
 	if (any(scaling_ind)) scaling = providedArgs[["scaling"]] else scaling = 1
 
+	if (any(label_ind))
+		par(mar = c(5, 4, 4, 4))
+	
 	plot(0, pch = "", xlim = c(0, n_iter), ylim = scaling*c(min_val, max_val), axes = TRUE, bg = "transparent",
 		xlab = ifelse(any(xlab_ind), providedArgs[["xlab"]], ""),
 		ylab = ifelse(any(ylab_ind), providedArgs[["ylab"]], ""),
 		main = ifelse(any(main_ind), providedArgs[["main"]], ""))
 
 	for (chain in 1:n_chains)
-		lines(1:n_iter, scaling*draws[, chain,], type = "l", col = cols[chain])
-		# lines(1:n_iter, draws[, chain,], type = "l", col = cols[chain])
+	{
+		if (all(class(draws) %in% c("draws_array", "draws", "array")))
+			lines(1:n_iter, scaling*draws[, chain,], type = "l", col = cols[chain])
+		if (is.array(draws) & !all(class(draws) %in% c("draws_array", "draws", "array")))
+			lines(1:n_iter, scaling*draws[, chain], type = "l", col = cols[chain])
+	}
+
 	if (any(val_ind))
+	{
+		for (val in ls_names[val_ind])
+			abline(h = scaling*providedArgs[[val]], col = "#CD212A", lwd = 4)
+		
+		if (any(label_ind))
 		{
-			for (val in ls_names[val_ind])
-				abline(h = scaling*providedArgs[[val]], col = "#CD212A", lwd = 4)
+			num_vals = stri_sub(str = ls_names[val_ind], from = stri_locate(str = ls_names[val_ind], regex = "val")[, "end"] + 1)
+			for (label in ls_names[label_ind])
+			{
+				num_label = stri_sub(str = label, from = stri_locate(str = label, regex = "label")[, "end"] + 1)
+				corresponding_val = (ls_names[val_ind])[num_vals == num_label]
+				axis(4, at = scaling*providedArgs[[corresponding_val]], providedArgs[[label]], las = 1)
+			}
 		}
+	}
 }
 
 #### Create dummy data
@@ -159,10 +179,10 @@ indices[, type := "child"]
 # Correct for those who are parent type
 indices[indices[, .I[which.min(year)], by = .(pointInventory_id, tree_id)][, V1], type := "parent"]
 
-# Compute nb years per individual
-indices[, nbYearsPerIndiv := max(year) - min(year) + 1, by = .(pointInventory_id, tree_id)]
+# Compute the number of growing years per individual
+indices[, nbYearsGrowth := max(year) - min(year), by = .(pointInventory_id, tree_id)]
 
-checkUp = all(indices[, nbYearsPerIndiv == index_clim_end - index_clim_start + 1])
+checkUp = all(indices[, nbYearsGrowth == index_clim_end - index_clim_start])
 if(!checkUp)
 	stop("Suspicious indexing. Review the indices data.table")
 
@@ -178,9 +198,9 @@ print(paste("Number of data:", n_obs))
 n_indiv = unique(treeData[, .(tree_id, pointInventory_id)])[, .N]
 print(paste("Number of individuals:", n_indiv))
 
-nbYearsPerIndiv = unique(indices[, .(tree_id, pointInventory_id, nbYearsPerIndiv)])[, nbYearsPerIndiv]
-if (length(nbYearsPerIndiv) != n_indiv)
-	stop("Dimension mismatch between nbYearsPerIndiv and n_indiv")
+nbYearsGrowth = unique(indices[, .(tree_id, pointInventory_id, nbYearsGrowth)])[, nbYearsGrowth]
+if (length(nbYearsGrowth) != n_indiv)
+	stop("Dimension mismatch between nbYearsGrowth and n_indiv")
 
 n_hiddenState = indices[.N, index_gen]
 
@@ -194,25 +214,24 @@ if (length(children_index) != n_obs - n_indiv)
 	stop("Dimension mismatch between children_index and number of children")
 
 ## Parameters (to be estimated). /!\ Given on a non standardised scale for dbh
-potentialGrowth = log(2.47); # i.e., in average a tree grows 2.47 cm
+potentialGrowth = log(4.47); # i.e., in average a tree grows 2.47 cm
 dbh_slope = 0.11/135;
 
 pr_slope = -0.34;
-pr_slope2 = 0.088;
+pr_slope2 = 0.0078;
 
 params = c(potentialGrowth = potentialGrowth, dbh_slope = dbh_slope, pr_slope = pr_slope, pr_slope2 = pr_slope2)
 
-processError = 5.68 # This is a variance: var /!\
-measurementError = sqrt(1) # This is a standard deviation: sd /!\ i.e., the variance is 3
+processError = 5.64 # This is a variance: var /!\
+measurementError = sqrt(3) # This is a standard deviation: sd /!\ i.e., the variance is 1
 
 ## Simulate dbh
 set.seed(123)
-n = treeData[, .N]/2
-if (n != length(children_index))
-	stop("Length mismatch between n and children_index")
+if (n_indiv != length(children_index))
+	stop("Length mismatch between n_indiv and children_index")
 
-latent_dbh = data.table(dbh0 = treeData[parents_index, dbh], dbh1 = numeric(n), dbh2 = numeric(n),
-	dbh3 = numeric(n), dbh4 = numeric(n), dbh5 = numeric(n))
+latent_dbh = data.table(dbh0 = treeData[parents_index, dbh], dbh1 = numeric(n_indiv), dbh2 = numeric(n_indiv),
+	dbh3 = numeric(n_indiv), dbh4 = numeric(n_indiv), dbh5 = numeric(n_indiv))
 ls_plot_id = unique(treeData[, pointInventory_id])
 
 clim_ind = indices[type == "parent", index_clim_start]
@@ -224,7 +243,7 @@ for (t in 1:5)
 	current_dbh = latent_dbh[, ..current_col]
 	setnames(current_dbh, new = "lala")
 	current_dbh = current_dbh[, lala]
-	latent_dbh[, (paste0("dbh", t)) := current_dbh + rgamma(n, shape = (growth_fct(current_dbh, precip, params))^2/processError,
+	latent_dbh[, (paste0("dbh", t)) := current_dbh + rgamma(n_indiv, shape = (growth_fct(current_dbh, precip, params))^2/processError,
 		rate = (growth_fct(current_dbh, precip, params))/processError)]
 	clim_ind = clim_ind + 1
 }
@@ -233,20 +252,26 @@ for (t in 1:5)
 treeData[parents_index, dbh := rnorm(.N, mean = latent_dbh[, dbh0], sd = measurementError)]
 treeData[children_index, dbh := rnorm(.N, mean = latent_dbh[, dbh5], sd = measurementError)]
 
-any(treeData[children_index, dbh] - treeData[parents_index, dbh] < 0)
+(checkData = any(treeData[children_index, dbh] - treeData[parents_index, dbh] < 0))
+if (checkData)
+{
+	problems = which(treeData[children_index, dbh] - treeData[parents_index, dbh] < 0)
+	print(treeData[c(parents_index[problems[1]], children_index[problems[1]])])
+	print(latent_dbh[problems[1]])
+}
 
 lm_pa = lm(treeData[parents_index, dbh] ~ latent_dbh[, dbh0])
 lm_ch = lm(treeData[children_index, dbh] ~ latent_dbh[, dbh5])
 
-summary(lm_pa) # Should be a slope of 1, an intercept of 0, and a residual of sqrt(1) = 1
-summary(lm_ch) # Should be a slope of 1, an intercept of 0, and a residual of sqrt(1) = 1
+summary(lm_pa) # Should be a slope of 1, an intercept of 0, and a residual of sqrt(3)
+summary(lm_ch) # Should be a slope of 1, an intercept of 0, and a residual of sqrt(3)
 
 (sd_dbh = treeData[, sd(dbh)]) # Do not forget to update it to the dummy data!
 
 #### Stan model
 ## Define stan variables
 # Common variables
-maxIter = 1200
+maxIter = 2000
 n_chains = 3
 
 # Initial value for states only
@@ -264,9 +289,9 @@ stanData = list(
 	n_indiv = n_indiv, # Number of individuals
 	n_precip = climate[, .N], # Dimension of the climate vector
 	n_obs = n_obs, # Number of trees observations
-	n_hiddenState = n_hiddenState, # Dimension of the state space vector
+	n_latentGrowth = n_hiddenState - n_indiv, # Dimension of the state space vector for latent growth
 	n_children = n_obs - n_indiv, # Number of children trees observations = n_obs - n_indiv
-	nbYearsPerIndiv = nbYearsPerIndiv, # Number of years for each individual
+	nbYearsGrowth = nbYearsGrowth, # Number of years for each individual
 
 	# Indices
 	parents_index = parents_index, # Index of each parent in the observed data
@@ -288,21 +313,28 @@ stanData = list(
 model = cmdstan_model("./growth_3rdOption.stan")
 
 ## Run model
+results = readRDS("./Tilia_platyphyllos/test_procErrorFixed_3rdOption.rds")
 results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 50, chains = n_chains,
 	iter_warmup = round(1*maxIter/3), iter_sampling = round(2*maxIter/3), save_warmup = TRUE, init = initVal_Y_gen,
 	max_treedepth = 13, adapt_delta = 0.9)
 
-results$save_output_files(dir = paste0("./", species, "/"), basename = "test_procErrorFixed_3rdOption_sqrt(1)",
+results$save_output_files(dir = paste0("./", species, "/"), basename = "test_procErrorFixed_3rdOption",
 	timestamp = FALSE, random = FALSE)
-results$save_object(file = paste0("./", species, "/test_procErrorFixed_3rdOption_sqrt(1).rds"))
+results$save_object(file = paste0("./", species, "/test_procErrorFixed_3rdOption.rds"))
 
 #### Check-up
 ## Flags
 results$cmdstan_diagnose()
 results$print()
 
+aa = results$draws("measureError")
+
+# pdf("measureError.pdf")
+lazyTrace(sd_dbh*aa, val1 = measurementError, val2 = mean(sd_dbh*aa), label1 = "real", label2 = "est")
+# dev.off()
+providedArgs = list(val1 = measurementError, val2 = mean(sd_dbh*aa), label1 = "real", label2 = "est")
 ## Check first five dbh
-row = 1:5
+row = 1:50
 
 estimated_parents = results$draws(paste0("latent_dbh_parents[", row,"]"))
 estimated_parents = apply(estimated_parents, 3, mean)
@@ -313,6 +345,7 @@ x = row
 ymin = min(sd_dbh*estimated_parents, real, measured)
 ymax = max(sd_dbh*estimated_parents, real, measured)
 
+pdf("50shadesOfDBH.pdf")
 op <- par(mar = c(2.5, 2.5, 0.8, 0.8), mgp = c(1.5, 0.5, 0), tck = -0.015)
 plot(x, sd_dbh*estimated_parents, pch = 19, col = "#34568B", cex = 4,
 	xlab = "Index", ylab = "Diameter at breast height (scaled)", ylim = c(ymin, ymax))
@@ -320,162 +353,4 @@ points(x, y = measured, pch = 19, col = "#FA7A35", cex = 2)
 points(x, real, pch = 19, col = "#CD212A", cex = 1.5)
 legend(x = "topleft", legend = c("Estimated", "Measured", "Real"), fill = c("#34568B", "#FA7A35", "#CD212A"),
 	box.lwd = 0)
-
-#### Compute residuals: compare data versus latent states with obs error
-n_rep = results$metadata()$iter_sampling * results$num_chains()
-
-dt_dharma = data.table(
-	rep_latent_id = c(rep(indices[type == "parent", index_gen], each = n_rep), rep(indices[type == "child", index_gen], each = n_rep)),
-	rep_dbh = c(rep(treeData[parents_index, dbh], each = n_rep), rep(treeData[children_index, dbh], each = n_rep)),
-	sampled = numeric(n_rep * treeData[, .N]))
-
-latent_dbh_parents_array = results$draws("latent_dbh_parents") # dimension: iter_sampling * n_chains * n_indiv
-latent_growth_array = results$draws("latent_growth") # dimension: iter_sampling * n_chains * (n_hiddenState - n_indiv)
-
-latent_dbh_array = compute_latent_dbh(latent_growth_array, latent_dbh_parents_array, indices, n_hiddenState)
-dt_dharma[, sampled := myPredictObs(latent_dbh_array, rep_latent_id), by = rep_latent_id]
-
-sims = matrix(data = dt_dharma[, sampled], nrow = n_rep, ncol = treeData[, .N]) # each column is for one data point
-sims = t(sims) # Transpose the matrix for dharma
-dim(sims)
-
-if (ncol(sims) != n_rep)
-	stop("Dimensions mismatch")
-
-if (nrow(sims) != length(unique(dt_dharma$rep_latent_id)))
-	stop("Dimensions mismatch")
-
-dt_dharma[, diff := rep_dbh/sd_dbh - sampled]
-meanDiff = dt_dharma[, mean(diff), by = rep_latent_id]
-print(paste0("The mean is ", round(meanDiff[, mean(V1)], digits = 8)))
-plot(meanDiff[, rep_latent_id], meanDiff[, V1], xlab = "Latent tree id", ylab = "Average difference: measured - simulated", pch = 19)
-
-index_parent_extreme = which(meanDiff[rep_latent_id %in% indices[type == "parent", index_gen], V1] > 0.01)
-latent_id_parent_extreme = meanDiff[index_parent_extreme, rep_latent_id]
-tree_point_id_extreme = indices[index_gen %in% latent_id_parent_extreme, .(tree_id, pointInventory_id)]
-latent_id_child_extreme = integer(tree_point_id_extreme[, .N])
-
-for (i in 1:tree_point_id_extreme[, .N])
-{
-	latent_id_child_extreme[i] = indices[(type == "child") & (tree_id == tree_point_id_extreme[i, tree_id]) &
-		(pointInventory_id == tree_point_id_extreme[i, pointInventory_id]), (index_gen)]
-}
-
-# pdf("meanDiff.pdf", height = 8, width = 8)
-plot(1:meanDiff[, .N], meanDiff[, V1], pch = 19, xlab = "Index", ylab = "Average difference: measured - simulated")
-abline(v = meanDiff[, .N]/2, col = "#CD212A", lwd = 4)
-points(which(meanDiff[, rep_latent_id] %in% c(latent_id_parent_extreme, latent_id_child_extreme)),
-	meanDiff[rep_latent_id %in% c(latent_id_parent_extreme, latent_id_child_extreme), V1], col = "#FA7A35", pch = 19, cex = 1.25)
-dev.off()
-
-hist(meanDiff[, V1])
-hist(dt_dharma[, diff])
-
-forDharma = createDHARMa(simulatedResponse = sims,
-	observedResponse = dt_dharma[seq(1, .N, by = n_rep), rep_dbh/sd_dbh]) # treeData[parents_index, dbh/sd_dbh]
-
-plot(forDharma)
-dev.off()
-
-
-## Check dbh of individual at line row < n_indiv
-row = 1
-
-latent = latent_dbh_array[, , paste0("latent_dbh[",
-	indices[type =="parent"][row, index_gen]:indices[type =="child"][row, index_gen],
-	"]")]
-latent = apply(latent, 3, mean)
-real = unlist(latent_dbh[row,])
-measured = c(treeData[parents_index[row], dbh], treeData[children_index[row], dbh])
-x = 2000:2005
-
-ymin = min(sd_dbh*latent, real, measured)
-ymax = max(sd_dbh*latent, real, measured)
-
-op <- par(mar = c(2.5, 2.5, 0.8, 0.8), mgp = c(1.5, 0.5, 0), tck = -0.015)
-plot(2000:2005, sd_dbh*latent, pch = 19, col = "#34568B", cex = 4,
-	xlab = "Years (fake)", ylab = "Diameter at breast height (scaled)", ylim = c(ymin, ymax))
-points(x = c(2000, 2005), y = measured, pch = 19, col = "#FA7A35", cex = 2)
-points(x, real, pch = 19, col = "#CD212A", cex = 1.5)
-legend(x = "topleft", legend = c("Estimated", "Measured", "Real"), fill = c("#34568B", "#FA7A35", "#CD212A"), box.lwd = 0)
-dev.off()
-
-#### Retrieve the parameters back
-# If there is no mistake (see notebook), then the relation between some stan parameters and the parameters for non standardised dbh is:
-#	potentialGrowth_stan = potentialGrowth_R - log(sd(dbh))
-#	slope_dbh_stan = sd(dbh)*slope_dbh_R,
-# Where *_stan are for the stan estimates, while *_R are for the real values in R
-# Note that there is no transformation required for the climate parameters, only the dbh_slope and the intercept are influenced
-
-
-
-
-
-
-#### 
-
-par(mfrow = c(2,1))
-lazyTrace(latent_dbh_array[, , paste0("latent_dbh[", indices[type =="parent"][row, index_gen], "]")],
-	val1 = treeData[parents_index[row], dbh]/sd(treeData[, dbh]), main = paste0("tree ", row, " at t0"), scaling = sd(treeData$dbh))
-lazyTrace(latent_dbh_array[, , paste0("latent_dbh[", indices[type =="child"][row, index_gen], "]")],
-	val1 = treeData[children_index[row], dbh]/sd(treeData[, dbh]), main = paste0("tree ", row, " at t1"), scaling = sd(treeData$dbh))
-dev.off()
-
-
-plot(dataGrowth[, dbh], dataGrowth[, growth], pch = 19)
-curve(growth_fct(x, params, 0, sd_dbh), col = "#178F92", lwd = 3, from = 0, to = 700, add = TRUE)
-
-lazyTrace(results$draws("potentialGrowth"))
-lazyTrace(results$draws("slope_dbh"))
-
-
-## Few trace plots
-lazyTrace(results$draws("potentialGrowth"), val1 = potentialGrowth)
-lazyTrace(results$draws("dbh_slope"), val1 = dbh_slope)
-lazyTrace(results$draws("pr_slope"), val1 = pr_slope)
-lazyTrace(results$draws("pr_slope2"), val1 = pr_slope2)
-
-lazyTrace(sd_dbh^2*results$draws("processError")) # processError is a variance, so should be multiplied by sd(dbh)^2
-lazyTrace(sd_dbh*results$draws("measureError")) # measureError is a standard deviation, so should be multiplied by sd(dbh)
-
-#### Check likelihood (for second test, see notebook)
-vec_pG = seq(potentialGrowth - 0.1, potentialGrowth + 0.1, length.out = 40)
-vec_dbhSlope = seq(dbh_slope - 0.008, dbh_slope + 0.012, length.out = 40)
-
-ll = matrix(data = 0, nrow = length(vec_pG), ncol = length(vec_dbhSlope))
-control = vector(mode = "list", length = ncol(ll)*nrow(ll))
-names(control) = paste0("iter_", 1:length(control))
-
-dim(ll)
-r = 1
-for (pG in vec_pG)
-{
-	c = 1
-	for (dbhSlope in vec_dbhSlope)
-	{
-		currentParams = c(potentialGrowth = pG, dbh_slope = dbhSlope, pr_slope = pr_slope, pr_slope2 = pr_slope2)
-		ll[r, c] = loglik(the_answer = the_answer/treeData[, sd(dbh)], n_indiv = n_indiv, n_hiddenState = indices[.N, index_gen],
-			nbYearsPerIndiv = nbYearsPerIndiv,
-			climate_index = indices[type == "parent", index_clim_start],
-			normalised_precip = (climate[, pr] - climate_mu_sd[1, mu])/climate_mu_sd[1, sd],
-			params = currentParams, processError = processError/treeData[, var(dbh)])
-		c = c + 1
-	}
-	if (r %% 5 == 0)
-		print(r)
-	r = r + 1
-}
-
-x_sol = mean(results$draws("dbh_slope"))
-y_sol = mean(results$draws("potentialGrowth"))
-x_real = params["dbh_slope"]
-y_real = params["potentialGrowth"]
-
-# pdf("./Tilia_platyphyllos/test_errorsFixed_latentGiven_prSlopesNull_seed=123.pdf")
-filled.contour(x = vec_dbhSlope, y = vec_pG, z = t(ll), xlab = "dbh slope", ylab = "potential growth",
-	plot.axes = {
-		axis(1); axis(2);
-		points(x_sol, y_sol, col = "#1D1F54", pch = 19, cex = 2); text(x_sol, y_sol, pos = 4, labels = "Estimated")
-		points(x_real, y_real, col = "#1D7FDF", pch = 19, cex = 2); text(x_real, y_real, pos = 4, labels = "Real solution")
-	})
 dev.off()
