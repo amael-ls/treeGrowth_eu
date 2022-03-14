ad6aee934e1d5a73180a1a81435026f135ea76dc
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-03-14 19:18:24 +0100

    Loaded the required data

--- plotGrowth.R
+++ plotGrowth.R
@@ -10,10 +10,11 @@ options(max.print = 500)
 library(data.table)
 library(cmdstanr)
 library(stringi)
+library(terra)
 
 #### Tool functions
 ## Growth function
-growth_fct = function(dbh, precipitation, temperature, basalArea, params, scaled_dbh = FALSE, scaled_clim = FALSE, ...)
+growth_fct = function(x, precipitation, temperature, basalArea, params, scaled_dbh = FALSE, scaled_clim = FALSE, ...)
 {
 	providedArgs = list(...)
 	providedArgs_names = names(providedArgs)
@@ -26,7 +27,7 @@ growth_fct = function(dbh, precipitation, temperature, basalArea, params, scaled
 		
 		sd_dbh = providedArgs[["sd_dbh"]]
 		scaling = sd_dbh
-		dbh = dbh/sd_dbh
+		x = x/sd_dbh
 	}
 
 	if (!scaled_clim)
@@ -48,6 +49,38 @@ growth_fct = function(dbh, precipitation, temperature, basalArea, params, scaled
 
 	competition_slope = params["competition_slope"]
 
-	return(scaling*exp(potentialGrowth + dbh_slope*dbh + pr_slope*precipitation + pr_slope2*precipitation^2 +
-			tas_slope*temperature + tas_slope2*temperature^2 + competition_slope*basalRea))
+	return(scaling*exp(potentialGrowth + dbh_slope*x + pr_slope*precipitation + pr_slope2*precipitation^2 +
+		tas_slope*temperature + tas_slope2*temperature^2 + competition_slope*basalRea))
 }
+
+#### Load data
+## Common variables
+species = "Tilia_platyphyllos"
+year = 2015
+
+## Paths
+tree_path = paste0("./", species, "/")
+climate_path = "/home/amael/project_ssm/climateData/Chelsa/yearlyAverage/"
+shapefile_path = "/home/amael/shapefiles/deutschland/"
+# shapefile_path = "/Users/mistral/Nextcloud/shapefiles/Germany_shapefile/"
+
+## Tree data and parameters
+results = readRDS(paste0(tree_path, "growth-2022-03-10_17h33.rds"))
+
+## Climate
+temperature_file = paste0(climate_path, "tas/", year, ".tif")
+precipitation_file = paste0(climate_path, "pr/", year, ".tif")
+
+climate = rast(c(temperature_file, precipitation_file))
+
+## Landscape
+germany = vect(paste0(shapefile_path, "germany.shp"))
+germany = simplifyGeom(x = germany, tolerance = 100)
+germany = project(x = germany, y = climate)
+
+## Crop climate to Germany only
+climate = crop(x = climate, y = germany, mask = TRUE)
+
+## Scaling
+dbh_mu_sd = readRDS(paste0(tree_path, "dbh_normalisation.rds"))
+climate_mu_sd = readRDS(paste0(tree_path, "climate_normalisation.rds"))
