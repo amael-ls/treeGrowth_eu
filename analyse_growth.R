0a566d324f8c937c92b2b3be36b5601625a8eb22
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-03-14 23:25:37 +0100

    Updated model to posterior generate quantites: added new parameters and growth fct. Changed filenames for all figures to account for run_id

--- analyse_growth.R
+++ analyse_growth.R
@@ -303,16 +303,18 @@ gq_script = write_stan_file(
 "
 functions {
 	// Function for growth. This returns the expected growth in mm, for 1 year.
-	real growth(real dbh0, real precip, real potentialGrowth, real dbh_slope, real pr_slope, real pr_slope2)
+	real growth(real dbh0, real precip, real temperature, real totalTreeWeight,
+		real potentialGrowth, real dbh_slope, real pr_slope, real pr_slope2 , real tas_slope, real tas_slope2, real competition_slope)
 	{
-		return (exp(potentialGrowth + dbh_slope*dbh0 + pr_slope*precip + pr_slope2*precip^2));
+		return (exp(potentialGrowth + dbh_slope*dbh0 + pr_slope*precip + pr_slope2*precip^2 +
+			tas_slope*temperature + tas_slope2*temperature^2 + competition_slope*totalTreeWeight));
 	}
 }
 
 data {
 	// Number of data
 	int<lower = 1> n_indiv; // Number of individuals
-	int<lower = 1> n_precip; // Dimension of the climate vector
+	int<lower = 1> n_climate; // Dimension of the climate vector
 	int<lower = 1> n_obs; // Number of trees observations
 	int<lower = 1> n_latentGrowth; // Dimension of the state space vector for latent growth
 	int<lower = n_obs - n_indiv, upper = n_obs - n_indiv> n_children; // Number of children trees observations = n_obs - n_indiv
@@ -321,22 +323,29 @@ data {
 	// Indices
 	array [n_indiv] int<lower = 1, upper = n_obs - 1> parents_index; // Index of each parent in the observed data
 	array [n_children] int<lower = 2, upper = n_obs> children_index; // Index of children in the observed data
-	array [n_indiv] int<lower = 1, upper = n_precip - 1> climate_index; // Index of the climate associated to each parent
+	array [n_indiv] int<lower = 1, upper = n_climate - 1> climate_index; // Index of the climate associated to each parent
 
 	// Observations
 	vector<lower = 0>[n_obs] Yobs;
 
 	// Explanatory variables
-	vector<lower = 0>[n_precip] precip; // Precipitations
+	vector<lower = 0>[n_climate] precip; // Precipitations
 	real pr_mu; // To standardise the precipitations
 	real<lower = 0> pr_sd; // To standardise the precipitations
 
-	vector<lower = 0>[n_obs] totalTrunkArea;
+	vector[n_climate] tas; // Temperature
+	real tas_mu; // To standardise the temperature
+	real<lower = 0> tas_sd; // To standardise the temperature
+
+	vector<lower = 0>[n_indiv] totalTreeWeight; // Sum of the tree weights for a given plot at a given tieme
 }
 
 transformed data {
 	vector[n_obs] normalised_Yobs = Yobs/sd(Yobs); // Normalised but NOT centred dbh
-	vector[n_precip] normalised_precip = (precip - pr_mu)/pr_sd; // Normalised and centered precipitations
+	vector[n_climate] normalised_precip = (precip - pr_mu)/pr_sd; // Normalised and centered precipitations
+	vector[n_climate] normalised_tas = (tas - tas_mu)/tas_sd; // Normalised and centered temperatures
+	vector[n_indiv] normalised_totalTreeWeight = (totalTreeWeight - mean(totalTreeWeight))/sd(totalTreeWeight);
+	// Normalised and centred totalTreeWeight
 }
 
 parameters {
@@ -346,6 +355,11 @@ parameters {
 
 	real pr_slope;
 	real pr_slope2;
+
+	real tas_slope;
+	real tas_slope2;
+
+	real competition_slope;
 
 	real<lower = 0.5/sd(Yobs)^2> processError;
 	real<lower = 0.1/sqrt(12)*25.4/sd(Yobs)> measureError; // Constrained by default, see appendix D Eitzel for the calculus
@@ -384,7 +398,8 @@ generated quantities {
 			{
 				// Process model
 				expected_growth = growth(current_latent_dbh, normalised_precip[climate_index[i] + j - 1],
-					potentialGrowth, dbh_slope, pr_slope, pr_slope2);
+					normalised_tas[climate_index[i] + j - 1], normalised_totalTreeWeight[i],
+					potentialGrowth, dbh_slope, pr_slope, pr_slope2, tas_slope, tas_slope2, competition_slope);
 
 				// Record difference expected growth versus latent growth
 				procError_sim[growth_counter] = expected_growth - latent_growth[growth_counter];
@@ -413,10 +428,10 @@ gq_model = cmdstan_model(gq_script)
 # Access data
 n_chains = results$num_chains()
 iter_sampling = results$metadata()$iter_sampling
-stanData = readRDS(paste0(path, "stanData.rds"))
+stanData = readRDS(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "stanData.rds"))
 n_obs = stanData$n_obs
 n_hiddenState = stanData$n_latentGrowth + stanData$n_indiv
-indices = readRDS(paste0(path, species, "_indices.rds"))
+indices = readRDS(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "indices.rds"))
 
 # Simulations
 generate_quantities = gq_model$generate_quantities(results$draws(), data = stanData, parallel_chains = n_chains)
@@ -446,20 +461,20 @@ dt_dharma[rep_id %in% stanData$children_index, type := "children"]
 
 setorderv(x = dt_dharma, cols = c("type", "rep_id"), order = c(-1, 1)) # The -1 is to have parents first
 
-jpeg("residuals_obs_scatter.jpg", quality = 100)
+jpeg(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "residuals_obs_scatter.jpg"), quality = 50)
 plot(dt_dharma[, rep_id], dt_dharma[, residuals_obs], pch = 19, col = "#A1A1A122")
 abline(v = stanData$n_indiv, lwd = 2, col = "#CD212A")
 axis(3, at = n_obs/4, "Parents", las = 1)
 axis(3, at = 3*n_obs/4, "Children", las = 1)
 dev.off()
 
-pdf("residuals_obs_hist.pdf")
+pdf(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "residuals_obs_hist.pdf"))
 hist(dt_dharma[, residuals_obs])
 dev.off()
 
 qq = qqnorm(dt_dharma[, residuals_obs], pch = 1, frame = FALSE, plot.it = FALSE)
 
-jpeg("qqplot.jpg")
+jpeg(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "qqplot.jpg"))
 plot(qq)
 qqline(dt_dharma[, residuals_obs], col = "#34568B", lwd = 3)
 dev.off()
@@ -473,7 +488,7 @@ dim(processError_array) # iter_sampling * n_chains * n_latentGrowth
 processError_vec = as.vector(processError_array) # The order is array[,1,1], array[,2,1], ..., array[,n_chain,1], array[,2,1], ...
 length(processError_vec)
 
-pdf("processError_check_hist.pdf")
+pdf(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "processError_check_hist.pdf"))
 hist(processError_vec)
 dev.off()
 
@@ -489,7 +504,7 @@ if (length(index_notLastMeasure) != length (processError_avg))
 latent_dbh = apply(generate_quantities$draws("yearly_latent_dbh"), 3, mean)
 latent_dbh = latent_dbh[index_notLastMeasure]
 
-pdf("processError_vs_dbh_check.pdf")
+pdf(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "processError_vs_dbh_check.pdf"))
 plot(latent_dbh, processError_avg, type = "l")
 dev.off()
 
