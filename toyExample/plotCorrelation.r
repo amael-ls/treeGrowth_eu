8872e703e4e0f9a4526a7c66dbfddf3c6ba2afc8
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-04-11 18:15:23 +0200

    Removed folder toyExample

--- toyExample/plotCorrelation.r
+++
@@ -1,8 +0,0 @@
-
-# library(BayesianTools)
-library(cmdstanr)
-
-results = readRDS("./toyPara_GPUs_nonInformative_noProcessError_23-08-2021_12h22.rds")
-
-mcmc_pairs(results$draws(), regex_pars = c("Error", "intercept"))
-
