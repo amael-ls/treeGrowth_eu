6219075b27611f21edb3781e07edebe42fdb8ea0
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-04-11 18:34:46 +0200

    Tracking file to prepare data from different countries for growth

---
+++ prepareGrowthData.R
@@ -0,0 +1,14 @@
+
+#### Aim of script: Prepare the data for growth (indices for multiple countries)
+## Comments
+#
+
+#### Clear memory and load packages
+rm(list = ls())
+graphics.off()
+
+options(max.print = 500)
+
+library(data.table)
+
+
