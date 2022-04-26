ab190db5ab8468af68e6a383ac1a7d2ff1be0c98
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-04-26 16:30:38 +0200

    Cleaned code, adapted the residuals to new model. Added a python plot to accelerate the script. Adapted index_notLastMeasure to trees measured more than twice

--- analyse_growth.R
+++ analyse_growth.R
@@ -8,8 +8,12 @@ graphics.off()
 options(max.print = 500)
 
 library(data.table)
+library(reticulate)
 library(cmdstanr)
 library(stringi)
+library(png)
+
+use_python("/usr/bin/python3")
 
 #### Tool function
 ## Get fixed values parameters
@@ -362,172 +366,8 @@ results$print(c("lp__", "averageGrowth_mu", "averageGrowth_sd", "dbh_slope", "pr
 #	2. Generate the parent observation from the corresponding latent dbh according to the model
 #	3. Generate the children observation according to the model (i.e., after n years of yearly latent growth)
 
-gq_script = write_stan_file(
-"
-functions {
-	// Function for growth. This returns the expected growth in mm, for 1 year.
-	real growth(real dbh0, real precip, real temperature /*, real ph*/, real standBasalArea, real averageGrowth, real dbh_slope,
-		real pr_slope, real pr_slope2 , real tas_slope, real tas_slope2 /* , real ph_slope, real ph_slope2 */, real competition_slope)
-	{
-		return (exp(averageGrowth + dbh_slope*dbh0 + pr_slope*precip + pr_slope2*precip^2 +
-			tas_slope*temperature + tas_slope2*temperature^2 /* + ph_slope*ph + ph_slope2*ph^2 */ + competition_slope*standBasalArea));
-	}
-}
-
-data {
-	// Number of data
-	int<lower = 1> n_indiv; // Total number of individuals (all NFIs together)
-	int<lower = 1> n_climate; // Dimension of the climate vector (all NFIs together)
-	int<lower = 1, upper = n_indiv> n_plots; // Number of plots (all NFIs together)
-	int<lower = 1> n_obs; // Total number of tree observations (all NFIs together)
-	int<lower = 1> n_latentGrowth; // Dimension of the state space vector for latent growth (all NFIs together)
-	int<lower = n_obs - n_indiv, upper = n_obs - n_indiv> n_children; // Number of children tree observations = n_obs - n_indiv
-	array [n_indiv] int<lower = 2, upper = n_obs> nbYearsGrowth; // Number of years of growth for each individual
-	int<lower = 1> n_inventories; // Number of forest inventories involving different measurement errors in the data
-
-	// Indices
-	array [n_indiv] int<lower = 1, upper = n_obs - 1> parents_index; // Index of each parent in the 'observation space'
-	array [n_children] int<lower = 2, upper = n_obs> children_index; // Index of children in the 'observation space'
-	array [n_children] int<lower = 2> latent_children_index; // Index of children in the 'latent space'
-	array [n_indiv] int<lower = 1, upper = n_climate - 1> climate_index; // Index of the climate associated to each parent
-	
-	array [n_indiv] int<lower = 1, upper = n_inventories> nfi_id; // Number of the NFI for a given individual
-	
-	array [n_indiv] int<lower = 1, upper = n_plots> plot_index; // Indicates to which plot individuals belong to
-
-	// Observations
-	vector<lower = 0>[n_obs] Yobs;
-
-	// sd_dbh is for the whole species and should not be more than 5% different from the sd of the subsample, namely sd(Yobs)
-	real<lower = 0.95*sd(Yobs), upper = 1.05*sd(Yobs)> sd_dbh;
-
-	// Explanatory variables
-	vector<lower = 0>[n_climate] precip; // Precipitations
-	real<lower = 0> pr_mu; // To standardise the precipitations
-	real<lower = 0> pr_sd; // To standardise the precipitations
-
-	vector[n_climate] tas; // Temperature
-	real tas_mu; // To standardise the temperature
-	real<lower = 0> tas_sd; // To standardise the temperature
-
-	// vector<lower = 0, upper = 14>[n_plots] ph; // pH of the soil measured with CaCl2
-	// real<lower = 0, upper = 14> ph_mu; // To standardise the pH
-	// real<lower = 0> ph_sd; // To standardise the pH
-
-	vector<lower = 0>[n_indiv] standBasalArea; // Sum of the tree basal area for a given plot at a given time
-}
-
-transformed data {
-	vector[n_obs] normalised_Yobs = Yobs/sd_dbh; // Normalised but NOT centred dbh
-	vector[n_climate] normalised_precip = (precip - pr_mu)/pr_sd; // Normalised and centred precipitations
-	vector[n_climate] normalised_tas = (tas - tas_mu)/tas_sd; // Normalised and centred temperatures
-	// vector[n_plots] normalised_ph = (ph - ph_mu)/ph_sd; // Normalised and centred pH
-	vector[n_indiv] normalised_standBasalArea = (standBasalArea - mean(standBasalArea))/sd(standBasalArea); // Normalised and centred BA
-}
-
-parameters {
-	// Parameters for growth function
-	array [n_plots] real averageGrowth; // Growth (grouped by plots) when all the explanatory variables are set to 0
-	real dbh_slope;
-
-	real pr_slope;
-	real pr_slope2;
-
-	real tas_slope;
-	real tas_slope2;
-
-	// real ph_slope;
-	// real ph_slope2;
-
-	real competition_slope;
-
-	// Hyper parameters for growth function
-	real averageGrowth_mu;
-	real<lower = 0> averageGrowth_sd;
-
-	// Errors (observation and process)
-	// --- Process error, which is the variance of a gamma distrib /!\
-	real<lower = 0.5/sd_dbh^2> sigmaProc;
-
-	// --- Routine observation error, which is constrained by default, see appendix D Eitzel for the calculus.
-	array [n_inventories] real<lower = 0.1/sqrt(12)*25.4/sd_dbh> sigmaObs; // Std. Dev. of a normal distrib /!\
-
-	// --- Extreme error, by default at least twice the observation error. Rüger 2011 found it is around 8 times larger
-	array [n_inventories] real<lower = 2*0.1/sqrt(12)*25.4/sd_dbh> etaObs; // Std. Dev. of a normal distrib /!\
-	
-	array [n_inventories] real<lower = 0, upper = 1> p; // probabilities of occurrence of extreme errors (etaObs) for each NFI
-
-	// Latent states
-	// --- Parent (i.e., primary) dbh
-	vector<lower = 0.1/sd_dbh, upper = 3000/sd_dbh>[n_indiv] latent_dbh_parents; // Real (and unobserved) first measurement dbh (parents)
-
-	// --- Growth
-	vector<lower = 0>[n_latentGrowth] latent_growth; // Real (and unobserved) yearly growth
-}
-
-generated quantities {
-	// Declaration output variables
-	array [n_obs] real newObservations;
-	array [n_obs] real latent_dbh_parentsChildren;
-	array [n_latentGrowth] real procError_sim;
-	array [n_latentGrowth + n_indiv] real yearly_latent_dbh;
-	
-	{
-		// Variables declared in nested blocks are local variables, not generated quantities, and thus won't be printed.
-		real current_latent_dbh;
-		int growth_counter = 1;
-		int dbh_counter = 1;
-		int children_counter = 1;
-		real expected_growth;
-
-		for (i in 1:n_indiv) // Loop over all the individuals
-		{
-			// Fill the parents
-			latent_dbh_parentsChildren[parents_index[i]] = latent_dbh_parents[i];
-			yearly_latent_dbh[dbh_counter] = latent_dbh_parents[i];
-
-			// Generate the parent observation conditional on the parent state
-			newObservations[parents_index[i]] = (1 - p[nfi_id[i]]) * normal_rng(latent_dbh_parents[i], sigmaObs[nfi_id[i]]) +
-				p[nfi_id[i]] * normal_rng(latent_dbh_parents[i], etaObs[nfi_id[i]]);
-
-			// Starting point to compute latent dbh child
-			current_latent_dbh = latent_dbh_parents[i];
-
-			for (j in 1:nbYearsGrowth[i]) // Loop for all years but the first (which is the parent of indiv i)
-			{
-				// Process model
-				expected_growth = growth(current_latent_dbh, normalised_precip[climate_index[i] + j - 1],
-					normalised_tas[climate_index[i] + j - 1] /*, normalised_ph[plot_index[i]]*/, normalised_standBasalArea[i],
-					averageGrowth[plot_index[i]], dbh_slope, pr_slope, pr_slope2, tas_slope, tas_slope2 /*, ph_slope, ph_slope2 */,
-					competition_slope);
-
-				// Record difference expected growth versus latent growth
-				procError_sim[growth_counter] = expected_growth - latent_growth[growth_counter];
-
-				// Dbh at time t + 1
-				current_latent_dbh += latent_growth[growth_counter]; // Or should it be += gamma(mean = latent_growth, var = sigmaProc)?
-				growth_counter += 1;
-				dbh_counter += 1;
-				yearly_latent_dbh[dbh_counter] = current_latent_dbh;
-
-				// Recording the children dbh
-				if (dbh_counter == latent_children_index[children_counter])
-				{
-					latent_dbh_parentsChildren[children_index[i]] = current_latent_dbh;
-					newObservations[children_index[i]] = (1 - proba[nfi_id[i]]) * normal_rng(current_latent_dbh, sigmaObs[nfi_id[i]]) +
-						proba[nfi_id[i]] * normal_rng(current_latent_dbh, etaObs[nfi_id[i]]);
-					children_counter += 1;
-				}
-			}
-			dbh_counter += 1;
-		}
-	}
-}
-"
-)
-
 ## Compile simulation generator
-gq_model = cmdstan_model(gq_script)
+gq_model = cmdstan_model("./generate_posteriorSimulations.stan")
 
 ## Generate simulations
 # Access data
@@ -537,39 +377,6 @@ n_obs = stanData$n_obs
 n_hiddenState = stanData$n_latentGrowth + stanData$n_indiv
 indices = readRDS(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "indices.rds"))
 
-indices[, nfi_index := 1]
-indices[stri_detect_regex(plot_id, "germany"), nfi_index := 2]
-
-indices[stri_detect_regex(plot_id, "france"), .N]
-indices[stri_detect_regex(plot_id, "germany"), .N]
-
-growth_counter = 1
-dbh_counter = 1
-children_counter = 1
-
-latent_children_index = indices[type == "child", index_gen]
-indices[, type := "child"]
-indices[indices[, .I[which.min(year)], by = .(plot_id, tree_id)][, V1], type := "parent"]
-length(latent_children_index)
-
-for (i in 1:n_indiv)
-{
-	for (j in 1:nbYearsGrowth[i])
-	{
-		growth_counter = growth_counter + 1;
-		dbh_counter = dbh_counter + 1;
-
-		if (dbh_counter == latent_children_index[children_counter])
-			children_counter = children_counter + 1;
-	}
-	dbh_counter = dbh_counter + 1;
-}
-
-growth_counter - stanData$n_latentGrowth
-dbh_counter - stanData$n_latentGrowth - n_indiv
-children_counter - length(latent_children_index)
-
-
 stanData$nfi_id = unique(indices[, .(plot_id, tree_id, nfi_index)])[, nfi_index]
 
 if (length(stanData$nfi_id) != stanData$n_indiv)
@@ -578,7 +385,6 @@ if (length(stanData$nfi_id) != stanData$n_indiv)
 # Simulations
 generate_quantities = gq_model$generate_quantities(results$draws(), data = stanData, parallel_chains = n_chains)
 dim(generate_quantities$draws()) # iter_sampling * n_chains * (2*n_obs + n_latentGrowth + n_hiddenState)
-# 1000 x 3 x 172 835
 
 ## Check that the observation residuals are ok. There should not be any difference between parents and children, and no pattern with dbh
 n_rep = iter_sampling * n_chains
@@ -590,15 +396,10 @@ dt_dharma = data.table(rep_id = rep(1:n_obs, each = n_rep),
 
 newObservations_array = generate_quantities$draws("newObservations")
 dim(newObservations_array) # iter_sampling * n_chains * n_obs
-
-chain1 = newObservations_array[, 1, ] # iter_sampling * n_obs 
-obs_1_chain_1 = newObservations_array[, 1, 14885] # iter_sampling
-sum(is.na(obs_1_chain_1))
+sum(is.na(newObservations_array))
 
 latent_dbh_array = generate_quantities$draws("latent_dbh_parentsChildren")
 dim(latent_dbh_array) # iter_sampling * n_chains * n_obs
-
-# There area NAs from 44 652 001 to 52 971 000, length = 4 161 000
 
 dt_dharma[, simulated_observations := reshapeDraws(newObservations_array, rep_id, regex = "newObservations"), by = rep_id]
 dt_dharma[, simulated_observations := sd_dbh*simulated_observations]
@@ -614,11 +415,21 @@ dt_dharma[rep_id %in% stanData$children_index, type := "children"]
 setorderv(x = dt_dharma, cols = c("type", "rep_id"), order = c(-1, 1)) # The -1 is to have parents first
 
 jpeg(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "residuals_obs_scatter.jpg"), quality = 50)
-plot(dt_dharma[, rep_id], dt_dharma[, residuals_obs], pch = 19, col = "#A1A1A122")
+plot(dt_dharma[, rep_id], dt_dharma[, residuals_obs], pch = '.', col = "#A1A1A122")
 abline(v = stanData$n_indiv, lwd = 2, col = "#CD212A")
 axis(3, at = n_obs/4, "Parents", las = 1)
 axis(3, at = 3*n_obs/4, "Children", las = 1)
 dev.off()
+
+mpl <- import("matplotlib")
+mpl$use("Agg") # Stable non interactive back-end
+plt <- import("matplotlib.pyplot")
+mpl$rcParams['agg.path.chunksize'] = 0 # Disable error check on too many points
+
+plt$figure()
+plt$plot(dt_dharma[, rep_id], dt_dharma[, residuals_obs], '.', c = "#A1A1A122", markersize = 1)
+plt$savefig(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "residuals_obs_scatter.png"))
+plt$close(plt$gcf())
 
 pdf(paste0(path, ifelse(!is.null(run), paste0(run, "_"), run), "residuals_obs_hist.pdf"))
 hist(dt_dharma[, residuals_obs])
@@ -631,7 +442,7 @@ plot(qq)
 qqline(dt_dharma[, residuals_obs], col = "#34568B", lwd = 3)
 dev.off()
 
-## Check that the process error does not show any pattern with any predictor, and that it is gamma distributed
+## Check that the process error does not show any pattern with any predictor
 sigmaProc_array = generate_quantities$draws("procError_sim")
 mean(sigmaProc_array)
 
@@ -648,9 +459,9 @@ sigmaProc_avg = apply(sigmaProc_array, 3, mean) # Average sigmaProc for each lat
 length(sigmaProc_avg)
 
 index_notLastMeasure = 1:n_hiddenState
-index_notLastMeasure = index_notLastMeasure[!(index_notLastMeasure %in% indices[stanData$children_index, index_gen])]
+index_notLastMeasure = index_notLastMeasure[!(index_notLastMeasure %in% indices[stanData$last_child_index, index_gen])]
 
-if (length(index_notLastMeasure) != length (sigmaProc_avg))
+if (length(index_notLastMeasure) != length(sigmaProc_avg))
 	stop("Dimension mismatch between sigmaProc_avg and index_notLastMeasure")
 
 latent_dbh = apply(generate_quantities$draws("yearly_latent_dbh"), 3, mean)
@@ -661,131 +472,3 @@ plot(latent_dbh, sigmaProc_avg, type = "l")
 dev.off()
 
 cor(latent_dbh, sigmaProc_avg)
-
-
-
-#############################
-
-
-gq_script = write_stan_file(
-"
-functions {
-	// Function for growth. This returns the expected growth in mm, for 1 year.
-	real growth(real dbh0, real precip, real temperature, real totalTreeWeight,
-		real averageGrowth, real dbh_slope, real pr_slope, real pr_slope2 , real tas_slope, real tas_slope2, real competition_slope)
-	{
-		return (exp(averageGrowth + dbh_slope*dbh0 + pr_slope*precip + pr_slope2*precip^2 +
-			tas_slope*temperature + tas_slope2*temperature^2 + competition_slope*totalTreeWeight));
-	}
-}
-
-data {
-	// Number of data
-	int<lower = 1> n_indiv; // Number of individuals
-	int<lower = 1> n_climate; // Dimension of the climate vector
-	int<lower = 1> n_obs; // Number of trees observations
-	int<lower = 1> n_latentGrowth; // Dimension of the state space vector for latent growth
-	int<lower = n_obs - n_indiv, upper = n_obs - n_indiv> n_children; // Number of children trees observations = n_obs - n_indiv
-	array [n_indiv] int<lower = 2, upper = n_obs> nbYearsGrowth; // Number of years of growth for each individual
-
-	// Indices
-	array [n_indiv] int<lower = 1, upper = n_obs - 1> parents_index; // Index of each parent in the observed data
-	array [n_children] int<lower = 2, upper = n_obs> children_index; // Index of children in the observed data
-	array [n_indiv] int<lower = 1, upper = n_climate - 1> climate_index; // Index of the climate associated to each parent
-
-	// Observations
-	vector<lower = 0>[n_obs] Yobs;
-
-	// Explanatory variables
-	vector<lower = 0>[n_climate] precip; // Precipitations
-	real pr_mu; // To standardise the precipitations
-	real<lower = 0> pr_sd; // To standardise the precipitations
-
-	vector[n_climate] tas; // Temperature
-	real tas_mu; // To standardise the temperature
-	real<lower = 0> tas_sd; // To standardise the temperature
-
-	vector<lower = 0>[n_indiv] totalTreeWeight; // Sum of the tree weights for a given plot at a given tieme
-}
-
-transformed data {
-	vector[n_obs] normalised_Yobs = Yobs/sd(Yobs); // Normalised but NOT centred dbh
-	vector[n_climate] normalised_precip = (precip - pr_mu)/pr_sd; // Normalised and centered precipitations
-	vector[n_climate] normalised_tas = (tas - tas_mu)/tas_sd; // Normalised and centered temperatures
-	vector[n_indiv] normalised_totalTreeWeight = (totalTreeWeight - mean(totalTreeWeight))/sd(totalTreeWeight);
-	// Normalised and centred totalTreeWeight
-}
-
-parameters {
-	// Parameters
-	real averageGrowth; // Growth when all the explanatory variables are set to 0
-	real dbh_slope;
-
-	real pr_slope;
-	real pr_slope2;
-
-	real tas_slope;
-	real tas_slope2;
-
-	real competition_slope;
-
-	real<lower = 0.5/sd(Yobs)^2> sigmaProc;
-	real<lower = 0.1/sqrt(12)*25.4/sd(Yobs)> sigmaObs; // Constrained by default, see appendix D Eitzel for the calculus
-
-	vector<lower = 0, upper = 10>[n_indiv] latent_dbh_parents; // Real (and unobserved) first measurement dbh (parents)
-	vector<lower = 0>[n_latentGrowth] latent_growth; // Real (and unobserved) yearly growth
-}
-
-generated quantities {
-	// Declaration output variables
-	array [n_obs] real newObservations;
-	array [n_obs] real latent_dbh_parentsChildren;
-	array [n_latentGrowth] real procError_sim;
-	array [n_latentGrowth + n_indiv] real yearly_latent_dbh;
-	
-	{
-		// Variables declared in nested blocks are local variables, not generated quantities, and thus won't be printed.
-		real current_latent_dbh;
-		int growth_counter = 1;
-		int dbh_counter = 1;
-		real expected_growth;
-
-		for (i in 1:n_indiv) // Loop over all the individuals
-		{
-			// Fill the parents
-			latent_dbh_parentsChildren[parents_index[i]] = latent_dbh_parents[i];
-			yearly_latent_dbh[dbh_counter] = latent_dbh_parents[i];
-
-			// Generate the parent observation conditional on the parent state
-			newObservations[parents_index[i]] = normal_rng(latent_dbh_parents[i], sigmaObs);
-
-			// Starting point to compute latent dbh child
-			current_latent_dbh = latent_dbh_parents[i];
-
-			for (j in 1:nbYearsGrowth[i]) // Loop for all years but the first (which is the parent of indiv i)
-			{
-				// Process model
-				expected_growth = growth(current_latent_dbh, normalised_precip[climate_index[i] + j - 1],
-					normalised_tas[climate_index[i] + j - 1], normalised_totalTreeWeight[i],
-					averageGrowth, dbh_slope, pr_slope, pr_slope2, tas_slope, tas_slope2, competition_slope);
-
-				// Record difference expected growth versus latent growth
-				procError_sim[growth_counter] = expected_growth - latent_growth[growth_counter];
-
-				// Dbh at time t + 1
-				current_latent_dbh += latent_growth[growth_counter]; // Or should it be += gamma(mean = latent_growth, var = sigmaProc)?
-				growth_counter += 1;
-				dbh_counter += 1;
-				yearly_latent_dbh[dbh_counter] = current_latent_dbh;
-			}
-
-			// The last current_latent_dbh corresponds to the child latent dbh
-			latent_dbh_parentsChildren[children_index[i]] = current_latent_dbh;
-			newObservations[children_index[i]] = normal_rng(latent_dbh_parentsChildren[children_index[i]], sigmaObs);
-			dbh_counter += 1;
-		}
-	}
-}
-"
-)
-
