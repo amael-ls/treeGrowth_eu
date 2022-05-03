768ddd44c33c6f4bcfd940392ab64a174a2b0200
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-05-03 19:19:02 +0200

    Coded extraction for path for indices and climate, and run. To finish: for path and run only

--- extractClimate.R
+++ extractClimate.R
@@ -31,4 +31,51 @@ extractClimate = function(...)
 
 		isConform = TRUE
 	}
+
+	if (all(c("path", "run") %in% ls_names))
+	{
+		print(paste("Using path and run. This extract climate for subsample", run))
+		
+		path = providedArgs[["path"]]
+		run = providedArgs[["run"]]
+
+		indices = readRDS(paste0(path, run, "_indices.rds"))
+		stanData = readRDS(paste0(path, run, "_stanData.rds"))
+		
+		#! TO FINISH HERE
+
+		isConform = TRUE
+	}
+
+	if (all(c("path_ind", "path_clim", "run") %in% ls_names))
+	{
+		print(paste("Using path for index and climate, and run. This extract climate for subsample", run))
+		
+		path = providedArgs[["path"]]
+		run = providedArgs[["run"]]
+
+		climate = readRDS(paste0(path_clim, "europe_reshaped_climate.rds"))
+		indices = readRDS(paste0(path_ind, run, "_indices.rds"))
+
+		indices = unique(indices[, .(plot_id, index_clim_start, index_clim_end)])
+		indices[, nbYears := index_clim_end - index_clim_start + 1]
+		tot_nbYears = indices[, sum(nbYears)]
+
+		extract_indices = integer(tot_nbYears)
+		end = 0
+		for (i in 1:indices[, .N])
+		{
+			start = end + 1
+			end = start + indices[i, nbYears] - 1
+			extract_indices[start:end] = indices[i, index_clim_start]:indices[i, index_clim_end]
+		}
+		
+		isConform = TRUE
+	}
+
+	if (!isConform)
+		stop("You must provide climate/indices together or path/run together")
+	
+
+
 }
