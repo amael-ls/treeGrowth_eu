0949f055d0ed984b518e96dbab763523a1f41202
lesa3111 <amael.le.squin@usherbrooke.ca>
2022-05-04 16:21:03 +0200

    Script done

--- infoSpecies.R
+++ infoSpecies.R
@@ -13,3 +13,24 @@ library(stringi)
 #### Load data
 treeData_folder = "/home/amael/project_ssm/inventories/growth/"
 treeData = readRDS(paste0(treeData_folder, "standardised_european_growth_data_reshaped.rds"))
+setindex(treeData, speciesName_sci)
+
+#### Create info data table
+## Species names and number of individuals
+info = unique(treeData[, .(speciesName_sci, plot_id, tree_id)])[, .(n_indiv = .N), by = speciesName_sci]
+
+## Order and set id which corresponds to the id used in the bash program
+setkey(info, speciesName_sci)
+info[, species_id := 1:.N]
+
+## Number of plots per species
+info[, n_plots := treeData[speciesName_sci, length(unique(plot_id)), on = "speciesName_sci"], by = speciesName_sci]
+
+## Number and list of NFIs per species
+info[, n_nfi := treeData[speciesName_sci, length(unique(nfi_id)), on = "speciesName_sci"], by = speciesName_sci]
+
+info[, ls_nfi := paste(treeData[speciesName_sci, unique(nfi_id), on = "speciesName_sci"], collapse = ", "), by = speciesName_sci]
+info[, ls_countries := paste(treeData[speciesName_sci, unique(country), on = "speciesName_sci"], collapse = ", "), by = speciesName_sci]
+
+#### Save informations
+saveRDS(info, "./speciesInformations.rds")
