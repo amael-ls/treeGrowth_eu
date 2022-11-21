d18113cc14f1242b748a4122fcef32addf24d854
amael-ls <amael.lesquin@gmail.com>
2022-11-21 19:23:42 +0100

    Data generator ready and running model

--- Aubry-Kientz2017.R
+++ Aubry-Kientz2017.R
@@ -9,6 +9,7 @@ options(max.print = 500)
 
 library(data.table)
 library(cmdstanr)
+library(stringi)
 
 #### Simulate data
 ## Common variables
@@ -39,3 +40,57 @@ for (i in 2:n_years)
 
 	treeData[, c(current_dbh) := (1 + beta1)*treeData[, ..previous_dbh] + beta0 + beta2*temperature[i]]
 }
+
+#### Stan model
+## Compile model
+model = cmdstan_model("Aubry-Kientz2017.stan")
+
+## Stan data
+# Common variables
+freq_obs = 2
+
+# Subset data
+keptCols = c("tree_id", paste0("dbh", seq(1, n_years, by = freq_obs)))
+treeData_obs = treeData[, ..keptCols]
+
+n_obs_year = length(keptCols) - 1 # -1 because I kept tree_id, which is not an observation
+
+# Compute the growth observations (averaged annual growth)
+counter = 1
+for (j in 3:length(keptCols))
+{
+	current_dbh = keptCols[j]
+	previous_dbh = keptCols[j - 1]
+	growthCol = paste0("growth", counter)
+
+	treeData_obs[, c(growthCol) := (treeData_obs[[current_dbh]] - treeData_obs[[previous_dbh]])/freq_obs]
+	counter = counter + freq_obs
+}
+growthCols = colnames(treeData_obs)[stri_detect(colnames(treeData_obs), regex = "growth[:digit:]")]
+
+# Stan list
+stanData = list(
+	# Number of data
+	n_indiv = n,
+	n_years = n_years,
+	n_obs_years = n_obs_year,
+
+	# Observations
+	dbh_init = treeData_obs[, dbh1],
+	observed_averaged_annual_growth = treeData_obs[, ..growthCols],
+	freq_obs = freq_obs,
+
+	# Predictors
+	temperature = temperature
+)
+
+## Run model
+n_chains = 1
+model = cmdstan_model("Aubry-Kientz2017.stan")
+results = model$sample(data = stanData, parallel_chains = n_chains, refresh = 50, chains = n_chains,
+	iter_warmup = 1500, iter_sampling = 1000, save_warmup = TRUE,
+	max_treedepth = 10, adapt_delta = 0.85)
+
+results$cmdstan_diagnose()
+
+results$print(c("lp__", "beta0", "beta1", "beta2", "sigmaProc"), max_rows = 20)
