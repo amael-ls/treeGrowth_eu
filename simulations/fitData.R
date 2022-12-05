536a54182fcbc5d2e59f286198ed1bc0bd9f7442
amael-ls <amael.lesquin@gmail.com>
2022-12-05 16:48:57 +0100

    Adapted stanData to new model, added plots for latent posterior growth

--- simulations/fitData.R
+++ simulations/fitData.R
@@ -12,7 +12,7 @@ library(cmdstanr)
 library(stringi)
 
 #### Tool functions
-source("../toolFunctions.R")
+source("./toolFunctions.R")
 
 #### Load data
 ## General data (list)
@@ -41,7 +41,7 @@ stanData = list(
 
 	# Observations
 	avg_yearly_growth_obs = data[["treeData"]][, ..growth_cols],
-	dbh_init = unlist(data[["treeData"]][, "dbh1"]),
+	dbh_init = unlist(data[["treeData"]][, "obs_dbh1"]),
 
 	# Explanatory variables
 	sd_dbh = data[["scaling"]]["sd_dbh_orig"], # To standardise the initial dbh
@@ -56,7 +56,10 @@ stanData = list(
 	beta2 = data[["parameters"]]["beta2"],
 
 	beta3 = data[["parameters"]]["beta3"],
-	beta4 = data[["parameters"]]["beta4"]
+	beta4 = data[["parameters"]]["beta4"],
+
+	# Observation error (not yet simulated in the data actually, just used for the likelihood)
+	sigmaObs = data[["sigmaObs"]]
 )
 
 ## Compile model
@@ -74,13 +77,26 @@ end_time = Sys.time()
 
 results$cmdstan_diagnose()
 
-lazyTrace(draws = results$draws("beta0"), val1 = data[["parameters"]]["beta0"])
-lazyTrace(draws = results$draws("beta1"), val1 = data[["parameters"]]["beta1"])
-lazyTrace(draws = results$draws("beta2"), val1 = data[["parameters"]]["beta2"])
-lazyTrace(draws = results$draws("beta3"), val1 = data[["parameters"]]["beta3"])
-lazyTrace(draws = results$draws("beta4"), val1 = data[["parameters"]]["beta4"])
-lazyTrace(draws = results$draws("sigmaProc"), val1 = data[["parameters"]]["sigmaProc"])
-lazyTrace(draws = results$draws("latent_dbh_parents[1]"), val1 = data[["treeData"]][1, dbh1]/data[["scaling"]]["sd_dbh_orig"])
+#### Plots
+## Lazy trace plots
+lazyTrace(draws = results$draws("beta0"), val1 = data[["parameters"]]["beta0"], main = paste("beta0 =", data[["parameters"]]["beta0"]))
+lazyTrace(draws = results$draws("beta1"), val1 = data[["parameters"]]["beta1"], main = paste("beta1 =", data[["parameters"]]["beta1"]))
+lazyTrace(draws = results$draws("beta2"), val1 = data[["parameters"]]["beta2"], main = paste("beta2 =", data[["parameters"]]["beta2"]))
+lazyTrace(draws = results$draws("beta3"), val1 = data[["parameters"]]["beta3"], main = paste("beta3 =", data[["parameters"]]["beta3"]))
+lazyTrace(draws = results$draws("beta4"), val1 = data[["parameters"]]["beta4"], main = paste("beta4 =", data[["parameters"]]["beta4"]))
+lazyTrace(draws = results$draws("sigmaProc"), val1 = data[["parameters"]]["sigmaProc"], main = "sigmaProc")
+lazyTrace(draws = results$draws("latent_dbh_parents[1]"),
+	val1 = data[["treeData"]][1, dbh1]/data[["scaling"]]["sd_dbh_orig"])
 
 results$save_output_files(dir = savingPath, basename = paste0(basename, "_diagnose"), timestamp = FALSE, random = TRUE)
 results$save_object(file = paste0(basename, "_results.rds"))
+
+indiv = 1
+
+for (i in 1:(data[["infos"]]["delta_t"] - 1))
+{
+	current_dbh = paste0("dbh", i)
+	next_dbh = paste0("dbh", i + 1)
+	lazyPosterior(draws = data[["scaling"]]["sd_dbh_orig"]*results$draws(paste0("latent_growth[", indiv, ",", i, "]")), fun = NULL,
+		val1 = data[["treeData"]][indiv, ..next_dbh] - data[["treeData"]][indiv, ..current_dbh])
+}
