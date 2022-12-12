9cb8e74a5c9402889d7e062eb74421b484e4546c
amael-ls <amael.lesquin@gmail.com>
2022-12-12 16:08:50 +0100

    Added obs error in the simulations of the data

--- simulations/createData.R
+++ simulations/createData.R
@@ -32,10 +32,10 @@ constrained_sum_sample_pos = function(n, total)
 ## Common variables
 set.seed(1969-08-18) # Woodstock seed
 
-n_indiv = 300
-n_plot = 5
+n_indiv = 2000
+n_plot = round(n_indiv/2.5) # German data have in average 2.5 individuals per plot
 n_measurements = 2 # Number of measurements per individual
-delta_t = 3 # Number of growing years between two measurements, e.g., 2000 --> 2003, 3 growing years 2000-2001, 2001-2002, 2002-2003
+delta_t = 10 # Number of growing years between two measurements, e.g., 2000 --> 2003, 3 growing years 2000-2001, 2001-2002, 2002-2003
 
 print(paste("In average there are", round(n_indiv/n_plot, 2), "individuals per plot"))
 
@@ -55,6 +55,7 @@ beta4 = -0.01 # Quadratic term
 
 # Variance
 sigmaProc = 0.42
+unscaled_sigmaObs = 2
 
 #### Create data
 ## Initial diameters
@@ -67,6 +68,8 @@ treeData[!(big_dbh), dbh1 := rgamma(n = .N, shape = 196^2/3678, rate = 196/3678)
 
 treeData[, big_dbh := NULL]
 sd_dbh_orig = treeData[, sd(dbh1)]
+
+sigmaObs = unscaled_sigmaObs/sd_dbh_orig
 
 ## Temperature
 temperature_avg = rnorm(n = n_annual_growth_per_indiv, mean = 9.7, sd = 0.3)
@@ -91,6 +94,8 @@ treeData[, tree_id := 1:.N, by = plot_id]
 
 all.equal(treeData[, .N, by = plot_id][, N], nb_trees_per_plot)
 
+# hist(treeData[, .N, by = plot_id][, N])
+
 ## Merge treeData and temperatures
 treeData = treeData[temperature, on = "plot_id"]
 
@@ -113,18 +118,31 @@ for (year in 1:n_annual_growth_per_indiv)
 ## Generate the observed growth (recorded only at frequency delta_t)
 dbh_recorded = paste0("dbh", seq(1, n_annual_growth_per_indiv + 1, by = delta_t))
 
+for (i in seq_along(dbh_recorded))
+{
+	current_dbh = dbh_recorded[i]
+	obs_dbh = paste0("obs_", current_dbh)
+
+	treeData[, c(obs_dbh) := rnorm(n = .N, mean = treeData[[current_dbh]], sd = unscaled_sigmaObs)]
+}
+
 for (i in seq_along(dbh_recorded[-length(dbh_recorded)]))
 {
 	current_dbh = dbh_recorded[i]
 	next_rec_dbh = dbh_recorded[i + 1]
-	obs_growth = paste0("growth", i)
 
-	treeData[, c(obs_growth) := (treeData[[next_rec_dbh]] - treeData[[current_dbh]])/delta_t]
+	obs_current_dbh = paste0("obs_", current_dbh)
+	obs_next_rec_dbh = paste0("obs_", next_rec_dbh)
+
+	obs_growth = paste0("obs_growth", i)
+
+	treeData[, c(obs_growth) := (treeData[[obs_next_rec_dbh]] - treeData[[obs_current_dbh]])/delta_t]
 }
 
 
 setcolorder(treeData, c("plot_id", "tree_id", paste0("dbh", 1:(n_annual_growth_per_indiv + 1)),
-	paste0("growth", seq_along(dbh_recorded[-length(dbh_recorded)])),
+	paste0("obs_", dbh_recorded),
+	paste0("obs_growth", seq_along(dbh_recorded[-length(dbh_recorded)])),
 	paste0("temperature", 1:n_annual_growth_per_indiv)))
 
 saveRDS(list(
@@ -132,5 +150,6 @@ saveRDS(list(
 		parameters = c(beta0 = beta0, beta1 = beta1, beta2 = beta2, beta3 = beta3, beta4 = beta4, sigmaProc = sigmaProc),
 		scaling = c(sd_dbh_orig = sd_dbh_orig, mu_temp = mu_temp, sd_temp = sd_temp),
 		infos = c(n_indiv = n_indiv, n_plot = n_plot, n_measurements = n_measurements, delta_t = delta_t,
-			n_annual_growth_per_indiv = n_annual_growth_per_indiv)
+			n_annual_growth_per_indiv = n_annual_growth_per_indiv),
+		sigmaObs = sigmaObs
 	), "dummyData.rds")
