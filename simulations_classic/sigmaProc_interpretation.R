adaf52e0b144270019c6f32513145a1b292fecdc
amael-ls <amael.lesquin@gmail.com>
2023-01-12 14:12:29 +0100

    Tracking script to compare sigmaProc (SSM approach) vs sigmaProc (classic approach)

---
+++ simulations_classic/sigmaProc_interpretation.R
@@ -0,0 +1,17 @@
+
+#### Aim of prog: Comparing sigmaProc (SSM approach) vs sigmaProc (classic approach)
+
+#### Clear memory and load packages
+rm(list = ls())
+graphics.off()
+
+options(max.print = 500)
+
+library(data.table)
+library(cmdstanr)
+library(stringi)
+
+#### Tool functions
+
+#### Load data
+
