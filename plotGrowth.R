f24c87f1a0b1d60daf9d9f72727c265ef2bc1e25
amael-ls <amael.lesquin@gmail.com>
2023-01-30 22:29:59 +0100

    Version that works for one individual (use i = 1 for instance). Do not run the for loop lines 345 -- 371, too slow

--- plotGrowth.R
+++ plotGrowth.R
@@ -2,7 +2,9 @@
 #### Aim of prog: Plot different visualisations of growth versus predictors (diameters, environmental factors)
 ## Comments
 #	The first part is dedicated to the growth response to environment and to time series predictions
-#	The second part is dedicated to 'cross-validation'
+#	The second part is dedicated to 'validation'
+#		For this part, I use radial increment based on wood cores (sampled with an increment borer) from the French data.
+#		These data have not been used to parametrise the growth model because they do not match the error structure used in our model
 #
 
 #### Clear memory and load packages
@@ -46,16 +48,374 @@ print(info)
 # Fagus sylvatica   germany_12591_3      10   2000   373
 # Fagus sylvatica   germany_12591_3      10   2012   437
 
-######## Part II: cross-validation
-
-
-
-
 ########! START CRASH TEST ZONE 1 ---------------------------------------------------------------------------------------------------------------
+######## Part II: validation
+#### Load data
+## All data (used later to keep exclusively french data that are not involved in the parametrisation of growth)
+treeFolder = "/home/amael/project_ssm/inventories/growth/"
+if (!dir.exists(treeFolder))
+	stop(paste0("Folder\n\t", treeFolder, "\ndoes not exist"))
+
+treeData = readRDS(paste0(treeFolder, "standardised_european_growth_data_reshaped.rds"))
+setkey(treeData, plot_id, tree_id, year)
+ls_species = sort(treeData[, unique(speciesName_sci)])
+
+## French data
+frenchFolder = "/bigdata/Inventories/FR IFN/processed data/"
+if (!dir.exists(frenchFolder))
+	stop(paste0("The folder <", frenchFolder, "> does not exist!"))
+
+frenchData = readRDS(paste0(frenchFolder, "trees_forest.rds"))
+stand_BA_dt = unique(readRDS(paste0(frenchFolder, "trees_forest_reshaped.rds"))[, .(pointInventory_id, year, standBasalArea)])
+
+#### Subset data
+## Keep only columns of interest and adapt classes or primary key columns to treeData classes
+keptCols = c("year", "tree_id", "pointInventory_id", "speciesName_sci", "dbh_in_mm", "state_1stVisit", "radial_increment_5_yrs_mm",
+	"xLambert93", "yLambert93")
+frenchData = frenchData[, ..keptCols]
+
+setnames(frenchData, old = "pointInventory_id", new = "plot_id")
+
+set(frenchData, j = "plot_id", value = as.character(frenchData[["plot_id"]]))
+set(frenchData, j = "tree_id", value = as.character(frenchData[["tree_id"]]))
+set(frenchData, j = "year", value = as.numeric(frenchData[["year"]]))
+
+frenchData[, plot_id := paste0("france_", plot_id)]
+frenchData = frenchData[year <= 2018] # Maximum year available for climate data
+
+setkey(frenchData, plot_id, tree_id, year)
+
+## Adapt classes or primary key columns to treeData classes
+setnames(stand_BA_dt, old = "pointInventory_id", new = "plot_id")
+set(stand_BA_dt, j = "plot_id", value = as.character(stand_BA_dt[["plot_id"]]))
+set(stand_BA_dt, j = "year", value = as.numeric(stand_BA_dt[["year"]]))
+
+stand_BA_dt[, plot_id := paste0("france_", plot_id)]
+stand_BA_dt = stand_BA_dt[year <= 2018] # Maximum year available for climate data
+
+## Remove non-parametrised species
+frenchData = frenchData[speciesName_sci %in% ls_species]
+
+## Remove the NAs radial increment
+frenchData = frenchData[!is.na(radial_increment_5_yrs_mm)]
+
+## Keep only living trees
+frenchData = frenchData[stri_detect(str = state_1stVisit, regex = "living")]
+
+## Remove the remaining french data involved in the parametrisation of the growth model
+frenchData = frenchData[!treeData]
+
+## Check uniqueness individuals (I assume it so that I can greatly simplify the algorithm to estimate previous dbh)
+uniquenessIndiv = all(frenchData[, .N, by = .(tree_id, plot_id)][, N] == 1)
+if (!uniquenessIndiv)
+	stop("I assumed that these trees have been measured only once!")
+
+frenchData = frenchData[speciesName_sci == species]
+
+#### Estimate the previous dbh
+## Comments:
+#	Because the last increment is not included, I need to estimate it. I assume the incomplete increment is 0.5 * incr/5
+#		The 0.5 comes from the fact that I assume the tree is measured at the middle of the growing season, while incr/5
+#		represents the averaged growth between t - 6 and t - 1, the year t being the current year for which the current
+#		dbh is measured. At the end, incr + 0.5 * incr/5 = 11/10 incr.
+#	Example: Imagine a tree is measured in June 2006. We can assume that the last tree ring is not complete yet, so the
+#		increment over the last 5 growing years corresponds to the growth between 2000 and 2005:
+#			1/ 2000 - 2001
+#			2/ 2001 - 2002
+#			3/ 2002 - 2003
+#			4/ 2003 - 2004
+#			5/ 2004 - 2005
+#			6/ 2006: dbh is measured, but the tree ring of that year is not complete and is not accounted in increment_5yrs
+
+## Dbh at t - 6
+# Compute
+frenchData[, dbh_6years_ago := dbh_in_mm - 11*radial_increment_5_yrs_mm/10]
+if (frenchData[, any(dbh_6years_ago < 0)])
+	warning("There are some negative dbh!")
+
+frenchData[, years_minus6 := year - 6]
+
+## Melting
+frenchData = melt(frenchData, measure = patterns("^dbh", "^year"), value.name = c("dbh", "year"))
+frenchData[, variable := NULL]
+
+## Check that now each individual has two lines
+if (!all(frenchData[, .N, by = .(tree_id, plot_id)][, N] == 2))
+	stop("Check the computation of the previous dbh! Maybe 'melt' failed")
+
+setkey(frenchData, plot_id, tree_id, year)
+
+growth = (frenchData[seq(2, .N, by = 2), dbh] - frenchData[seq(1, .N, by = 2), dbh])/6
+print(paste("Range of growth (in mm/year):", paste(round(range(growth), 2), collapse = " -> ")))
+
+## Remove useless columns, reorder
+frenchData[, c("state_1stVisit", "speciesName_sci") := NULL]
+setcolorder(frenchData)
+
+#### Get the environment time series (climate, soil pH, basal area) for this dataset
+## Function to get all the years within a time range
+getYears = function(time_range)
+{
+	years = stri_split(str = time_range, regex = "-", simplify = TRUE)
+	year_1 = as.integer(years[1])
+	year_2 = as.integer(years[2])
+
+	return(as.character((year_1:year_2)))
+}
+
+## Function to linearly interpolate stand basal area
+linearInterp = function(t, t0, t1, y0, y1)
+	return((y1 - y0)/(t1 - t0)*(t - t0) + y0)
+
+## Get time-plot_id coordinates
+frenchCoords = unique(frenchData[, .(plot_id, xLambert93, yLambert93)])
+index_min = unique(frenchData[frenchData[, .(m = .I[year == min(year)]), by = .(plot_id, xLambert93, yLambert93)]$m,
+	.(plot_id, xLambert93, yLambert93, year)])
+index_max = unique(frenchData[frenchData[, .(M = .I[year == max(year)]), by = .(plot_id, xLambert93, yLambert93)]$M,
+	.(plot_id, xLambert93, yLambert93, year)])
+
+frenchCoords[index_min, on = "plot_id", min_year := i.year]
+frenchCoords[index_max, on = "plot_id", max_year := i.year]
+frenchCoords[, unique_year_id := paste(min_year, max_year, sep = "-")]
+
+## Common variables
+selectedVariables = c("pr", "tas", "tasmin", "tasmax")
+clim_folder = "/bigdata/Predictors/climateChelsa/"
+soil_folder = "/bigdata/Predictors/Soil\ esdacph\ Soil\ pH\ in\ Europe/"
+
+## Compute length vector (sum of the number of years required for each plot)
+length_clim = frenchCoords[, sum(max_year - min_year + 1)]
+combination_years = unique(frenchCoords[, unique_year_id])
+
+clim_dt = data.table()
+
+for (j in selectedVariables)
+	set(clim_dt, i = NULL, j = j, value = rep(0, length_clim))
+
+clim_dt[, plot_id := "0"]
+clim_dt[, year := 0]
+
+count_clim_var = 0
+
+## Stack raster climate
+treeYears = sort(unique(frenchData[, year]))
+for (clim_var in selectedVariables)
+{
+	clim_rs = rast(x = paste0(clim_folder, clim_var, "/", treeYears, ".tif"))
+	names(clim_rs) = as.character(treeYears)
+	count = 0
+	start_ind = 1
+	
+	# Extraction
+	for (time_range_id in combination_years)
+	{
+		count = count + 1
+		selectedCoords = vect(frenchCoords[unique_year_id == time_range_id, .(xLambert93, yLambert93)], crs = "EPSG:2154",
+			geom = c("xLambert93", "yLambert93")) # Order MUST be longitude, latitude
+		selectedCoords = project(selectedCoords, "EPSG:4326")
+		selectedPoint_id = frenchCoords[unique_year_id == time_range_id, plot_id]
+		selectedLayers = getYears(time_range_id)
+		
+		values = setDT(extract(x = terra::subset(x = clim_rs, subset = selectedLayers), y = selectedCoords))
+		values[, plot_id := selectedPoint_id]
+		values = data.table::melt(data = values, id.vars = c("plot_id"), measure.vars = selectedLayers,
+			variable.name = "year", value.name = "climate", variable.factor = FALSE)
+		values[, year := as.integer(year)]
+		end_ind = start_ind + values[, .N] - 1
+		clim_dt[start_ind:end_ind, c("plot_id", "year") := values[, .(plot_id, year)]]
+		clim_dt[start_ind:end_ind, c(clim_var) := values[, climate]]
+
+		print(paste0(round(count*100/length(combination_years)), "% done for variable ", clim_var))
+		start_ind = end_ind + 1
+	}
+	count_clim_var = count_clim_var + 1
+	print(paste0("--->", round(count_clim_var*100/length(selectedVariables)), "% total done"))
+}
+
+setkey(clim_dt, plot_id, year)
+setcolorder(clim_dt)
+
+## Soil data. According to https://esdac.jrc.ec.europa.eu/content/soil-ph-europe, the projection is ETRS89 Lambert Azimuthal Equal Area 
+soil = rast(paste0(soil_folder, "ph_cacl2/w001001.adf"))
+soil_shp = vect(paste0(soil_folder, "country_laea.shp"))
+crs(soil) = crs(soil_shp)
+
+soil = project(x = soil, y = "EPSG:4326")
+
+selectedCoords = vect(frenchCoords[, .(plot_id, xLambert93, yLambert93)], crs = "EPSG:2154",
+	geom = c("xLambert93", "yLambert93")) # Order MUST be longitude, latitude
+selectedCoords = project(selectedCoords, "EPSG:4326")
+
+soil_values = extract(x = soil, y = selectedCoords, xy = TRUE, cell = TRUE)
+sum(is.na(soil_values))
+setDT(soil_values)
+
+# Few values are NA (the offset of some forest plots fall into the ocean, river, lakes...)
+problematicPlots = soil_values[is.na(ph_cacl2)]
+
+if (problematicPlots[, .N] != 0)
+{
+	for (i in 1:problematicPlots[, .N])
+		problematicPlots[i, ph_cacl2 := mean(soil[adjacent(x = soil, cells = problematicPlots[i, cell])]$ph_cacl2, na.rm = TRUE)]
+
+	sum(is.na(problematicPlots))
+	soil_values[problematicPlots[, ID], ph_cacl2 := problematicPlots[, ph_cacl2]]
+}
+
+soil_values[, plot_id := selectedCoords$plot_id]
+soil_values[, c("ID", "cell", "x", "y") := NULL]
+
+setnames(soil_values, old = "ph_cacl2", new = "ph")
+setkey(soil_values, plot_id)
+setcolorder(soil_values)
+
+## Merge climate with stand basal area and soil data
+env = stand_BA_dt[clim_dt, on = c("plot_id", "year")]
+env = env[soil_values, on = "plot_id"]
+
+## Replace NAs in stand basal area by the unique non NA value of each plot
+setkey(env, plot_id, year)
+
+env[, isUniqueBA := sum(!is.na(standBasalArea)) == 1, by = plot_id]
+if (any(env[, !isUniqueBA]))
+	stop("I assumed that there is only one non NA basal area!")
+
+env[, standBasalArea := env[plot_id][!is.na(standBasalArea), standBasalArea], by = plot_id]
+
+#### Run prediction based on the French data
+## Comments
+# The aim is to validate the fitted model. For this, I run predictions on the French data. The starting dbh has been estimated previously,
+#	and the ending dbh is measured. The climate data are ready so that we can extract the time series of the environmental conditions, including
+#	soil and stand basal area.
+# From https://mc-stan.org/cmdstanr/reference/model-method-generate-quantities.html
+#	if you plan on making many calls to $generate_quantities() then the most efficient option is to pass the paths of the CmdStan CSV output
+#	files (this avoids CmdStanR having to rewrite the draws contained in the fitted model object to CSV each time). If you no longer have the CSV
+#	files you can use draws_to_csv() once to write them and then pass the resulting file paths to $generate_quantities() as many times as needed.
+
+## Common variables
+tree_path = paste0("./", species, "/")
+if (!dir.exists(tree_path))
+	stop(paste0("Path not found for species <", species, ">."))
+
+## Load stand-alone generate_quantities() stan models
+gq_model_ssm = cmdstan_model("generate_posterior.stan")
+gq_model_classic = cmdstan_model("generate_posterior_classic.stan")
+
+## Load and start prepare stan data
+stanData_ssm = readRDS(paste0(tree_path, run, "_stanData.rds"))
+stanData_classic = readRDS(paste0(tree_path, run, "_stanData_classic.rds"))
+
+stanData_ssm$n_climate_new = 7
+stanData_ssm$n_years = 6
+stanData_ssm$n_growth = stanData_ssm$n_children
+
+stanData_classic$n_climate_new = 7
+stanData_classic$n_years = 6
+
+## List of csv files
+info_lastRun = getLastRun(path = tree_path, begin = "^growth-", extension = "_main.rds$", format = "ymd", run = run, hour = TRUE)
+ssm = readRDS(paste0(tree_path, info_lastRun[["file"]]))
+# csv_ssm = list.files(path = tree_path, pattern = paste0("^growth-run=", run, "-", info_lastRun[["time_ended"]], "_", info_lastRun[["hour"]],
+# 	".*.csv$"))
+# csv_ssm = paste0(tree_path, csv_ssm)
+
+info_lastRun = getLastRun(path = tree_path, begin = "^growth-", extension = "_classic.rds$", format = "ymd", run = run, hour = TRUE)
+classic = readRDS(paste0(tree_path, info_lastRun[["file"]]))
+# csv_classic = list.files(path = tree_path, pattern = paste0("^growth-run=", run, "-", info_lastRun[["time_ended"]], "_", info_lastRun[["hour"]],
+# 	".*.csv$"))
+# csv_classic = paste0(tree_path, csv_classic)
+
+n_chains = readRDS(paste0(tree_path, info_lastRun[["file"]]))$num_chains() # Same number for SSM, but classic approach is faster to load
+
+## For loop on individuals
+for (i in seq(1, frenchData[, .N], 2))
+{
+	# Preparing new stan data
+	# --- Common variables
+	time_span = frenchData[i, year]:frenchData[i + 1, year]
+	currentPlot = frenchData[i, plot_id]
+	
+	# --- Initial diameter
+	stanData_ssm$dbh0 = frenchData[i, dbh]
+	stanData_classic$dbh0 = frenchData[i, dbh]
+
+	# --- Add the new environment (x_r)
+	# ------ Approach: ssm
+	precipitations = (env[.(currentPlot, time_span), pr] - stanData_ssm$pr_mu)/stanData_ssm$pr_sd
+	temperatures = (env[.(currentPlot, time_span), tas] - stanData_ssm$tas_mu)/stanData_ssm$tas_sd
+	ph = (env[.(currentPlot, time_span), ph] - stanData_ssm$ph_mu)/stanData_ssm$ph_sd
+	basalAreas = (env[.(currentPlot, time_span), standBasalArea] - stanData_ssm$ba_mu)/stanData_ssm$ba_sd
+
+	stanData_ssm$x_r = c(precipitations, temperatures, ph, basalAreas)
+
+	# ------ Approach: classic
+	precipitations = (env[.(currentPlot, time_span), pr] - stanData_classic$pr_mu)/stanData_classic$pr_sd
+	temperatures = (env[.(currentPlot, time_span), tas] - stanData_classic$tas_mu)/stanData_classic$tas_sd
+	ph = (env[.(currentPlot, time_span), ph] - stanData_classic$ph_mu)/stanData_classic$ph_sd
+	basalAreas = (env[.(currentPlot, time_span), standBasalArea] - stanData_classic$ba_mu)/stanData_classic$ba_sd
+
+	stanData_classic$x_r = c(precipitations, temperatures, ph, basalAreas)
+	stanData_classic$x_r_avg = c(mean(precipitations), mean(temperatures), mean(ph), mean(basalAreas))
+
+	# Run simulations
+	generate_quantities_ssm = gq_model_ssm$generate_quantities(ssm$draws(), data = stanData_ssm, parallel_chains = n_chains)
+	generate_quantities_classic = gq_model_classic$generate_quantities(classic$draws(), data = stanData_classic, parallel_chains = n_chains)
+}
+
+simulatedGrowth_ssm = stanData_ssm$sd_dbh*generate_quantities_ssm$draws(paste0("current_dbh[", stanData_ssm$n_climate_new, "]"))
+simulatedGrowth_classic = stanData_classic$sd_dbh*generate_quantities_classic$draws(paste0("current_dbh[",
+	stanData_classic$n_climate_new, "]"))
+simulatedGrowth_classic_avg = stanData_classic$sd_dbh*generate_quantities_classic$draws("final_dbh")
+
+simulatedGrowth_avg_ssm = generate_quantities_ssm$draws("simulatedGrowth_avg")
+simulatedGrowth_avg_classic = generate_quantities_classic$draws("simulatedGrowth_avg")
+simulatedGrowth_avg_clim_avg = generate_quantities_classic$draws("simulatedGrowth_avg_clim_avg")
+
+# --- Compute the 2.5 and 97.5 quantiles due to uncertainty on parameters (no process error!)
+simulatedGrowth_avg_ssm_q2.5 = stanData_ssm$sd_dbh * apply(X = simulatedGrowth_avg_ssm, FUN = quantile, MARGIN = 3, probs = 0.025)
+simulatedGrowth_avg_ssm_q97.5 = stanData_ssm$sd_dbh * apply(X = simulatedGrowth_avg_ssm, FUN = quantile, MARGIN = 3, probs = 0.975)
+simulatedGrowth_avg_ssm = stanData_ssm$sd_dbh * apply(X = simulatedGrowth_avg_ssm, FUN = mean, MARGIN = 3)
+
+simulatedGrowth_avg_classic_q2.5 = stanData_ssm$sd_dbh *
+	apply(X = simulatedGrowth_avg_classic, FUN = quantile, MARGIN = 3, probs = 0.025)
+simulatedGrowth_avg_classic_q97.5 = stanData_ssm$sd_dbh *
+	apply(X = simulatedGrowth_avg_classic, FUN = quantile, MARGIN = 3, probs = 0.975)
+simulatedGrowth_avg_classic = stanData_ssm$sd_dbh * apply(X = simulatedGrowth_avg_classic, FUN = mean, MARGIN = 3)
+
+n_growingYears = stanData_classic$n_years
+simulatedGrowth_avg_clim_avg_q2.5 = stanData_ssm$sd_dbh/n_growingYears *
+	apply(X = simulatedGrowth_avg_clim_avg, FUN = quantile, MARGIN = 3, probs = 0.025)
+simulatedGrowth_avg_clim_avg_q97.5 = stanData_ssm$sd_dbh/n_growingYears *
+	apply(X = simulatedGrowth_avg_clim_avg, FUN = quantile, MARGIN = 3, probs = 0.975)
+simulatedGrowth_avg_clim_avg = stanData_ssm$sd_dbh/n_growingYears * apply(X = simulatedGrowth_avg_clim_avg, FUN = mean, MARGIN = 3)
+
+year_start = time_span[1]
+year_end = time_span[n_growingYears]
+
+plot(year_start:year_end, simulatedGrowth_avg_ssm, type = "l", xlab = "Year", ylab = "Growth", lwd = 2, col = "#F4C430",
+	ylim = c(min(simulatedGrowth_avg_ssm_q2.5), max(simulatedGrowth_avg_ssm_q97.5)))
+polygon(c(rev(year_start:year_end), year_start:year_end), c(rev(simulatedGrowth_avg_ssm_q2.5), simulatedGrowth_avg_ssm_q97.5),
+	col = "#F4C43022", border = NA)
+legend(x = "topleft", legend = "SSM", fill = "#F4C430", box.lwd = 0)
+
+lines(year_start:year_end, simulatedGrowth_avg_classic, lwd = 2, col = "#034C4F")
+polygon(c(rev(year_start:year_end), year_start:year_end), c(rev(simulatedGrowth_avg_classic_q2.5), simulatedGrowth_avg_classic_q97.5),
+	col = "#034C4F22", border = NA)
+abline(h = simulatedGrowth_avg_clim_avg, lwd = 2, lty = 2)
+legend(x = "topleft", legend = "Classic", fill = "#034C4F", box.lwd = 0)
+
+
+incr_ssm = stanData_ssm$sd_dbh*(generate_quantities_ssm$draws("current_dbh[6]") - generate_quantities_ssm$draws("current_dbh[1]"))
+incr_classic = stanData_classic$sd_dbh*(generate_quantities_classic$draws("current_dbh[6]") - generate_quantities_classic$draws("current_dbh[1]"))
+
+residuals_ssm = incr_ssm - frenchData[i, radial_increment_5_yrs_mm]
+residuals_classic = incr_classic - frenchData[i, radial_increment_5_yrs_mm]
+
+lazyComparePosterior(list(residuals_ssm,residuals_classic), legend = c("ssm", "classic"))
 
 
 ########! END CRASH TEST ZONE 1 -----------------------------------------------------------------------------------------------------------------
-
+22
 
 
 ########! START CRASH TEST ZONE 2 ---------------------------------------------------------------------------------------------------------------
