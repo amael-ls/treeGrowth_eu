f40cd0362fa858e50c3fdeb676ec1a0769fc73c2
amael-ls <amael.lesquin@gmail.com>
2023-09-22 14:13:20 +0200

    Tracking analyseResults

---
+++ simulations_aubry-kientz/analyseResults.R
@@ -0,0 +1,11 @@
+
+#### Aim of prog: Plot the posterior of the parameters given delta t
+
+#### Clear memory and load packages
+rm(list = ls())
+graphics.off()
+
+options(max.print = 500)
+
+library(data.table)
+library(cmdstanr)
