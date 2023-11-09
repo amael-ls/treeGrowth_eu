b15fbb1bb5051720276e8f8fafce279d43e25574
amael-ls <amael.lesquin@gmail.com>
2023-11-09 16:57:07 +0100

    Updated comments, changed the for loop to 1..45, the whole species list

--- run_growth_subsample.sh
+++ run_growth_subsample.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "You are running $0. If you have not done it yet, maybe use tmux to open a new window"
-echo "Make sure that there are at least 30 CPUs available."
-echo "You have 10 seconds to cancel $0 bash script in case you did not check CPUs or would like to use tmux"
+echo "You are running $0. If you have not done it yet, maybe you should check how many species you are trying to run"
+echo "Make sure that there are enough CPUSs left available for others"
+echo "You have 10 seconds to cancel $0 bash script in case you did not check CPUs or would like to change the species runned"
 
 sleep 10
 
-echo "Starting first run, the other runs will start 45 seconds later in background"
+echo "Starting first run, the other runs will start 45 seconds later (to let enough time to the first run to create the folders)"
 
 today=$(date +"%Y-%m-%d_%T")
 
-for species_id in 2 5 17 48
+for species_id in {1..45} # Maybe run by chunk of 5, except if your computer can handle 45 species x 4 runs x 4 chains!
 do
 	/usr/bin/Rscript --vanilla growth_subsample.R $species_id 1 15000 > ${species_id}_1_${today}.txt 2>&1 &
 done
 
 sleep 45
 
-for species_id in 17
+for species_id in {1..45} # Maybe run by chunk of 5, except if your computer can handle 45 species x 4 runs x 4 chains!
 do
 	for run_id in {2..4}
 	do
